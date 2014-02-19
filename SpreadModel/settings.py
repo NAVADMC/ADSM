4113b177231104db852838b54d4e529017cd1e44
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-02-19 12:03:34 -0700

    Working on auto-generated models from CreateTables.sql written by Melissa.

--- SpreadModel/settings.py
+++ SpreadModel/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'SpreadModel.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'test.db'),#'db.sqlite3'
     }
 }
 
