1c33b6fbdd76155060ee5ad96a1b4bed5fa01a0c
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-02-20 16:20:48 -0700

    Updating test to store reused scenario parameters separately.

--- ScenarioCreator/tests.py
+++ ScenarioCreator/tests.py
@@ -1,30 +1,31 @@
 from django.test import TestCase
 
 # Create your tests here.
-from ScenarioCreator.models import Scenario
+from ScenarioCreator.models import Ingeneral
 
+updatedPost = {'description': 'Updated Description', "naadsm_version": '3.2.19', "language": 'en', "num_runs": '10',
+     "num_days": '40', 'scenario_name': 'sample'}
+standardPost = {'description': 'words', "naadsm_version": '3.2.19', "language": 'en', "num_runs": '10',
+                        "num_days": '40', 'scenario_name': 'sample'}
 
 class CreatorTest(TestCase):
     def test_index_page(self):
         r = self.client.get('/setup/')
         self.assertEqual(r.status_code, 200)
 
-    def test_create_scenario(self):
+    def test_create_Ingeneral(self):
         r = self.client.get('/setup/new/')
         self.assertEqual(r.status_code, 200)
         self.assertTrue('form' in r.context)
-        length = Scenario.objects.count()
-        r = self.client.post('/setup/new/', {'description':'words', "naadsm_version":'3.2.19', "language":'en',
-                                             "num_runs":'10', "num_days":'40', 'scenario_name':'sample'})#TODO complete top level spec
+        length = Ingeneral.objects.count()
+        r = self.client.post('/setup/new/', standardPost)
         #"exit_condition":...
         self.assertEqual(r.status_code, 200)
-        self.assertEqual(Scenario.objects.count(), length +1)
+        self.assertEqual(Ingeneral.objects.count(), length +1)
 
-    def test_edit_scenario(self):
-        r = self.client.post('/setup/new/', {'description':'words', "naadsm_version":'3.2.19', "language":'en',
-                                             "num_runs":'10', "num_days":'40', 'scenario_name':'sample'})#TODO complete top level spec
+    def test_edit_Ingeneral(self):
+        r = self.client.post('/setup/new/', standardPost)
         r = self.client.get('/setup/1/')
-        r = self.client.post('/setup/1/', {'description':'Updated Description', "naadsm_version":'3.2.19', "language":'en',
-                                             "num_runs":'10', "num_days":'40', 'scenario_name':'sample'})
-        self.assertEqual(Scenario.objects.get(pk=1).description, 'Updated Description')
+        r = self.client.post('/setup/1/', updatedPost)
+        self.assertEqual(Ingeneral.objects.get(pk=1).description, 'Updated Description')
 
