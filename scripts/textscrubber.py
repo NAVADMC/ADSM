6cbe6cdc09a90b33d973dddb2b620bf94fe5637a
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-02-25 15:06:30 -0700

    textscrubber working on boolean field conversion.

--- scripts/textscrubber.py
+++ scripts/textscrubber.py
@@ -20,7 +20,7 @@ def lowercase_a_file(filename):
     #     print(text, end='')
 
 
-def hide_fields(filename, output_filename):
+def generate_forms_with_hidden_fields(filename, output_filename):
     form_lines = []
     excluded_fields = []
     model_ = ''
@@ -46,20 +46,21 @@ def hide_fields(filename, output_filename):
     open(output_filename, 'w').write('\n'.join(form_lines))
 
 
-def switch_to_boolean_field(filename):
-    lines = ['', '']
+def switch_to_boolean_fields(filename):
     lines = open(filename, 'r').readlines()
     for index, line in enumerate(lines):
-        if line.lstrip().find('use_') == 0:  # field name starts with 'use_'
-            field_name = line.split()[0]
+        field_name = line.split()[0] if line.split() else ''
+        if field_name.find('use_') != -1 or field_name.find('save_') != -1 :  # field name starts with 'use_' or 'save_'
             line_ending = line[line.find('(') + 1:]  # starting after the first paren til the end
-            lines[index] = field_name + " = models.BooleanField(default=False, " + line_ending
+            line_ending = line_ending.replace('blank=True, null=True', '')
+            lines[index] = '    ' + field_name + " = models.BooleanField(default=False, " + line_ending
 
-    open(filename, 'w').writelines(lines)
+    open(filename.replace('.py','')+'-out.py', 'w').writelines(lines)
 
 
 if __name__ == '__main__':
+    #Step #1:  Search:  db_column='[^']*', in models.py to remove column names
     print("Running from: ", os.getcwd())
     # lowercase_a_file('CreateDjangoInputTables.txt')
-    hide_fields('auto-models.py', 'auto-forms.py')
-    # switch_to_boolean_field('auto-models.py')
+    # generate_forms_with_hidden_fields('auto-models.py', 'auto-forms.py')
+    switch_to_boolean_fields('auto-models.py')
