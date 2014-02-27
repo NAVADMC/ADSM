a1ee4e45e139a4a6f03ed91cbccc99040ec496bd
josiahseaman <josiah.seaman@gmail.com>
2014-02-27 14:42:24 -0700

    Added URLs for /herd/new/

--- ScenarioCreator/views.py
+++ ScenarioCreator/views.py
@@ -7,21 +7,25 @@ def start_window(request):
     context = {'motd':'Remember your quotes!'}
     return render(request, 'ScenarioCreator/index.html', context)
 
-
-def new_scenario(request):
-    initialized_form = InGeneralForm(request.POST or None)
+def new_form(request, formClass, title):
+    initialized_form = formClass(request.POST or None)
     if initialized_form.is_valid():
-        initialized_form.save();  # write to database
-    context = {'form': initialized_form}
-    context['title'] = "Create a new scenario"
+        initialized_form.save()  # write to database
+    context = {'form': initialized_form,
+               'title': title}
     return render(request, 'ScenarioCreator/new.html', context)
 
+def new_scenario(request):
+    return new_form(request, InGeneralForm, "Create a new scenario")
+
+def new_herd(request):
+    return new_form(request, DynamicHerdForm, "Create a new Herd")
 
 def edit_scenario(request, primary_key):
     scenario = get_object_or_404(InGeneral, pk=primary_key)
     form = InGeneralForm(request.POST or None, instance=scenario)
     if request.method == 'POST' and form.is_valid():
         form.save(); #write to database
-    context = {'form': form}
-    context['title'] = "Edit a new scenario"
+    context = {'form': form,
+               'title': "Edit a new scenario"}
     return render(request, 'ScenarioCreator/new.html', context)
