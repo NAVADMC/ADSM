09f42a911160ee807324d68d1c49cde0f2558708
josiahseaman <josiah.seaman@gmail.com>
2014-02-28 10:37:19 -0700

    Committing auto-models for the purposes of diff.

--- scripts/auto-models.py
+++ scripts/auto-models.py
@@ -9,10 +9,9 @@
 # CharFields are max=255 characters and are presented as a single text line in forms.
 # TextFields are unlimited and presented as giant text boxes in forms.
 # Search:  db_column='[^']*',  to remove column names
-from __future__ import unicode_literals
 
 from django.db import models
-
+from django_extras.db.models import PercentField, LatitudeField, LongitudeField
 
 def chc(choice_list):
     return tuple((x, x) for x in choice_list)
@@ -30,12 +29,20 @@ class DynamicBlob(models.Model):
     zone_perimeters = models.CharField(max_length=255, blank=True)
 
 
-class DynamicHerd(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    initial_state_code = models.CharField(max_length=255, )
-    days_in_initial_state = models.IntegerField()
-    days_left_in_initial_state = models.IntegerField()
+class DynamicUnit(models.Model):
+    #ProductionType?
+    latitude = LatitudeField()
+    longitude = LongitudeField()
+    initial_state_code = models.CharField(max_length=255,
+                                          choices=(('L', 'Latent'),
+                                                   ('S', 'Susceptible'),
+                                                   ('B', 'Subclinical'),
+                                                   ('C', 'Clinical'),
+                                                   ('N', 'Naturally Immune'),
+                                                   ('V', 'Vaccine Immune'),
+                                                   ('D', 'Destroyed')))
+    days_in_initial_state = models.IntegerField(blank=True, null=True)
+    days_left_in_initial_state = models.IntegerField(blank=True, null=True)
     initial_size = models.IntegerField()
     _final_state_code = models.CharField(max_length=255, blank=True)
     _final_control_state_code = models.CharField(max_length=255, blank=True)
@@ -51,10 +58,15 @@ class DynamicHerd(models.Model):
 
 
 class InChart(models.Model):
-    field_name = models.CharField(max_length=255, blank=True)
+    field_name = models.CharField(max_length=255, )
     chart_name = models.CharField(max_length=255, )
-    _ispdf = models.BooleanField()
-    chart_type = models.CharField(max_length=255, blank=True)
+    _ispdf = models.BooleanField(default=True, )  # Set by the way the user enters the screen
+    chart_type = models.CharField(max_length=255, blank=True,
+                                  choices=chc("Point", "Uniform", "Triangular", "Piecewise", "Histogram", "Gaussian",
+                                              "Poisson", "Beta", "Gamma", "Weibull", "Exponential", "Pearson5",
+                                              "Logistic",
+                                              "LogLogistic", "Lognormal", "NegativeBinomial", "Pareto", "Bernoulli",
+                                              "Binomial", "Discrete Uniform", "Hypergeometric", "Inverse Gaussian"))
     mean = models.FloatField(blank=True, null=True)
     std_dev = models.FloatField(blank=True, null=True)
     min = models.FloatField(blank=True, null=True)
@@ -65,7 +77,7 @@ class InChart(models.Model):
     beta = models.FloatField(blank=True, null=True)
     location = models.FloatField(blank=True, null=True)
     scale = models.FloatField(blank=True, null=True)
-    shape = models.FloatField(blank=True, null=True)
+    shape = models.FloatField(blank=True, null=True)  # or should this be the chart_type list of PDF functions?
     n = models.IntegerField(blank=True, null=True)
     p = models.FloatField(blank=True, null=True)
     m = models.IntegerField(blank=True, null=True)
@@ -75,14 +87,14 @@ class InChart(models.Model):
     theta = models.FloatField(blank=True, null=True)
     a = models.FloatField(blank=True, null=True)
     s = models.IntegerField(blank=True, null=True)
-    x_axis_units = models.CharField(max_length=255, blank=True)
+    x_axis_units = models.CharField(max_length=255, default="Days")
     y_axis_units = models.CharField(max_length=255, blank=True)
-    _notes = models.TextField(blank=True)
+    _notes = models.TextField(blank=True, null=True)
 
 
 class InChartDetail(models.Model):
-    _chartid = models.IntegerField()
-    _pointorder = models.IntegerField()
+    _chart_id = models.IntegerField()
+    _point_order = models.IntegerField()
     _x = models.FloatField()
     _y = models.FloatField()
 
@@ -90,21 +102,21 @@ class InChartDetail(models.Model):
 class InControlGlobal(models.Model):
     include_detection = models.BooleanField(default=False, )
     include_tracing = models.BooleanField(default=False, )
-    include_tracing_herd_exam = models.BooleanField(default=False, )
+    include_tracing_unit_exam = models.BooleanField(default=False, )
     include_tracing_testing = models.BooleanField(default=False, )
     include_destruction = models.BooleanField(default=False, )
     destruction_delay = models.IntegerField(blank=True, null=True)
-    _destrcapacityrelid = models.IntegerField(blank=True, null=True)
-    destruction_priority_order = models.CharField(max_length=255, blank=True)
+    _destruction_capacity_relid = models.IntegerField(blank=True, null=True)
+    destruction_priority_order = models.CharField(max_length=255, blank=True)  # These are an odd legacy.  Leave it for now
     destrucion_reason_order = models.CharField(max_length=255, blank=True)
     include_vaccination = models.BooleanField(default=False, )
     vaccincation_detected_units_before_start = models.IntegerField(blank=True, null=True)
-    _vacccapacityrelid = models.IntegerField(blank=True, null=True)
+    _vaccination_capacity_relid = models.IntegerField(blank=True, null=True)
     vaccination_priority_order = models.CharField(max_length=255, blank=True)
     include_zones = models.BooleanField(default=False, )
     vaccination_retrospective_days = models.IntegerField(blank=True, null=True)
-    _vacccapacitystartrelid = models.IntegerField(blank=True, null=True)
-    _vacccapacityrestartrelid = models.IntegerField(blank=True, null=True)
+    _vaccination_capacity_start_relid = models.IntegerField(blank=True, null=True)
+    _vaccination_capacity_restart_relid = models.IntegerField(blank=True, null=True)
 
 
 class InControlPlan(models.Model):
@@ -116,17 +128,17 @@ class InControlPlan(models.Model):
 class InControlsProductionType(models.Model):
     production_type_id = models.IntegerField(unique=True)
     use_detection = models.BooleanField(default=False, )
-    _detprobobsvstimeclinicalrelid = models.IntegerField(blank=True, null=True)
-    _detprobreportvsfirstdetectionrelid = models.IntegerField(blank=True, null=True)
+    _detection_probability_for_observed_time_in_clinical_relid = models.IntegerField(blank=True, null=True)
+    _detection_probability_report_vs_first_detection_relid = models.IntegerField(blank=True, null=True)
     trace_direct_forward = models.BooleanField(default=False, )
     trace_direct_back = models.BooleanField(default=False, )
-    trace_direct_success = models.FloatField(blank=True, null=True)
+    trace_direct_success = PercentField(blank=True, null=True)
     trace_direct_trace_period = models.BooleanField(default=False, )
     trace_indirect_forward = models.BooleanField(default=False, )
     trace_indirect_back = models.BooleanField(default=False, )
-    trace_indirect_success = models.FloatField(blank=True, null=True)
+    trace_indirect_success = PercentField(blank=True, null=True)
     trace_indirect_trace_period = models.BooleanField(default=False, )
-    _tracedelaypdfid = models.IntegerField(blank=True, null=True)
+    _trace_delay_pdfid = models.IntegerField(blank=True, null=True)
     use_destruction = models.BooleanField(default=False, )
     destruction_is_ring_trigger = models.BooleanField(default=False, )
     destruction_ring_radius = models.FloatField(blank=True, null=True)
@@ -140,7 +152,7 @@ class InControlsProductionType(models.Model):
     vaccination_min_time_between = models.IntegerField(blank=True, null=True)
     vaccinate_detected = models.BooleanField(default=False, )
     days_to_immunity = models.IntegerField(blank=True, null=True)
-    _vaccimmuneperiodpdfid = models.IntegerField(blank=True, null=True)
+    _vaccine_immune_period_pdfid = models.IntegerField(blank=True, null=True)
     vaccinate_ring = models.BooleanField(default=False, )
     vaccination_ring_radius = models.FloatField(blank=True, null=True)
     vaccination_priority = models.IntegerField(blank=True, null=True)
@@ -170,7 +182,7 @@ class InControlsProductionType(models.Model):
     test_indirect_back = models.BooleanField(default=False, )
     test_specificity = models.FloatField(blank=True, null=True)
     test_sensitivity = models.FloatField(blank=True, null=True)
-    _testdelaypdfid = models.IntegerField(blank=True, null=True)
+    _test_delay_pdfid = models.IntegerField(blank=True, null=True)
     vaccinate_restrospective_days = models.BooleanField(default=False, )
 
 
@@ -182,15 +194,15 @@ class InDiseaseGlobal(models.Model):
 class InDiseaseProductionType(models.Model):
     _production_type_id = models.IntegerField(blank=True, null=True)
     use_disease_transition = models.BooleanField(default=False, )
-    _dislatentperiodpdfid = models.IntegerField(blank=True, null=True)
-    _dissubclinicalperiodpdfid = models.IntegerField(blank=True, null=True)
-    _disclinicalperiodpdfid = models.IntegerField(blank=True, null=True)
-    _disimmuneperiodpdfid = models.IntegerField(blank=True, null=True)
-    _disprevalencerelid = models.IntegerField(blank=True, null=True)
+    _disease_latent_period_pdfid = models.IntegerField(blank=True, null=True)  # TODO: All of these should be ForeignKeys
+    _disease_subclinical_period_pdfid = models.IntegerField(blank=True, null=True)
+    _disease_clinical_period_pdfid = models.IntegerField(blank=True, null=True)
+    _disease_immune_period_pdfid = models.IntegerField(blank=True, null=True)
+    _disease_prevalence_relid = models.IntegerField(blank=True, null=True)
 
 
 class InDiseaseSpread(models.Model):
-    _productiontypepairid = models.IntegerField(unique=True)
+    _production_type_pair_id = models.IntegerField(unique=True)#ForeignKey(InProductionType)
     spread_method_code = models.CharField(max_length=255, blank=True)
     latent_can_infect = models.BooleanField(default=False, )
     subclinical_can_infect = models.BooleanField(default=False, )
@@ -198,9 +210,9 @@ class InDiseaseSpread(models.Model):
     use_fixed_contact_rate = models.BooleanField(default=False, )
     fixed_contact_rate = models.FloatField(blank=True, null=True)
     infection_probability = models.FloatField(blank=True, null=True)
-    _distancepdfid = models.IntegerField(blank=True, null=True)
-    _movementcontrolrelid = models.IntegerField(blank=True, null=True)
-    _transportdelaypdfid = models.IntegerField(blank=True, null=True)
+    _distance_pdfid = models.IntegerField(blank=True, null=True)
+    _movement_control_relid = models.IntegerField(blank=True, null=True)
+    _transport_delay_pdfid = models.IntegerField(blank=True, null=True)
     probability_airborne_spread_1km = models.FloatField(blank=True, null=True)
     max_distance_airborne_spread = models.FloatField(blank=True, null=True)
     wind_direction_start = models.IntegerField(blank=True, null=True)
@@ -221,7 +233,7 @@ class InGeneral(models.Model):
     include_contact_spread = models.BooleanField(default=False, )
     include_airborne_spread = models.BooleanField(default=False, )
     use_airborne_exponential_decay = models.BooleanField(default=False, )
-    use_within_herd_prevalence = models.BooleanField(default=False, )
+    use_within_unit_prevalence = models.BooleanField(default=False, )
     cost_track_destruction = models.BooleanField(default=False, )
     cost_track_vaccination = models.BooleanField(default=False, )
     cost_track_zone_surveillance = models.BooleanField(default=False, )
@@ -229,13 +241,13 @@ class InGeneral(models.Model):
     random_seed = models.IntegerField(blank=True, null=True)
     ## Outputs requested:
     save_all_daily_outputs = models.BooleanField(default=False, )
-    save_daily_outputs_for_iterations = models.BooleanField(default=False, )
-    write_daily_states_file = models.BooleanField(default=False, 
-      help_text='The number of units in each state.  This variable always reports the counts on the day of reporting, regardless of whether it is reported daily, weekly, or at some other interval.  This variable is needed to create a plot of the states over time.')
+    maximum_iterations_for_daily_output = models.IntegerField(default=3, )
+    write_daily_states_file = models.BooleanField(default=False,
+      help_text='The number of units in each state.  This always reports the counts on the day of reporting, regardless of whether it is reported daily, weekly, or at some other interval.  This variable is needed to create a plot of the states over time.')
     daily_states_filename = models.CharField(max_length=255, blank=True)
     save_daily_events = models.BooleanField(default=False, )
     save_daily_exposures = models.BooleanField(default=False, )
-    save_iteration_outputs_for_herds = models.BooleanField(default=False, )
+    save_iteration_outputs_for_units = models.BooleanField(default=False, )
     write_map_output = models.BooleanField(default=False, )
     map_directory = models.CharField(max_length=255, blank=True)
 
@@ -246,14 +258,14 @@ class InProductionType(models.Model):
 
 
 class InProductionTypePair(models.Model):
-    _sourceproductiontypeid = models.IntegerField()
-    _destproductiontypeid = models.IntegerField()
+    _source_production_type_id = models.ForeignKey()
+    _destination_production_type_id = models.IntegerField()
     use_direct_contact = models.BooleanField(default=False, )
-    _directcontactspreadid = models.IntegerField(blank=True, null=True)
+    _direct_contact_spread_id = models.IntegerField(blank=True, null=True)
     use_indirect_contact = models.BooleanField(default=False, )
-    _indirectcontactspreadid = models.IntegerField(blank=True, null=True)
+    _indirect_contact_spread_id = models.IntegerField(blank=True, null=True)
     use_airborne_spread = models.BooleanField(default=False, )
-    _airbornecontactspreadid = models.IntegerField(blank=True, null=True)
+    _airborne_contact_spread_id = models.IntegerField(blank=True, null=True)
 
 
 class InZone(models.Model):
@@ -262,12 +274,12 @@ class InZone(models.Model):
 
 
 class InZoneProductionTypePair(models.Model):
-    _zoneid = models.IntegerField()
+    _zone_id = models.IntegerField()
     _production_type_id = models.IntegerField()
     use_directmovement_control = models.BooleanField(default=False, )
-    _zonedirectmovementrelid = models.IntegerField(blank=True, null=True)
+    _zone_direct_movement_relid = models.IntegerField(blank=True, null=True)
     use_indirect_movement_control = models.BooleanField(default=False, )
-    _zoneindirectmovementrelid = models.IntegerField(blank=True, null=True)
+    _zone_indirect_movement_relid = models.IntegerField(blank=True, null=True)
     use_detection_multiplier = models.BooleanField(default=False, )
     zone_detection_multiplier = models.FloatField(blank=True, null=True)
     cost_surv_per_animal_day = models.FloatField(blank=True, null=True)
