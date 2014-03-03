0f96cb4b6a210bdd001035dce8d2d5c2c3a36193
josiahseaman <josiah.seaman@gmail.com>
2014-03-03 12:27:59 -0700

    Fixing formatting of Results/models

--- Results/models.py
+++ Results/models.py
@@ -1,15 +1,5 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Remove `managed = False` lines for those models you wish to give write DB access
-# Feel free to rename the models, but don't rename db_table values or field names.
-#
-# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
-# into your database.
-from __future__ import unicode_literals
-
 from django.db import models
+
 
 class Outdailybyproductiontype(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
@@ -164,12 +154,14 @@ class Outdailybyproductiontype(models.Model):
     zone_new_foci = models.IntegerField(blank=True, null=True)
     zone_cum_foci = models.IntegerField(blank=True, null=True)
 
+
 class Outdailybyzone(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
     day = models.IntegerField(blank=True, null=True)
     zone_id = models.IntegerField(blank=True, null=True)
     zone_area = models.FloatField(blank=True, null=True)
     zone_perimeter = models.FloatField(blank=True, null=True)
+
 
 class Outdailybyzoneandproductiontype(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
@@ -181,15 +173,17 @@ class Outdailybyzoneandproductiontype(models.Model):
     units_in_zone = models.IntegerField(blank=True, null=True)
     animals_in_zone = models.IntegerField(blank=True, null=True)
 
+
 class Outdailyevents(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
     day = models.IntegerField(blank=True, null=True)
     event = models.IntegerField(blank=True, null=True)
     herd_id = models.IntegerField(blank=True, null=True)
     zone_id = models.IntegerField(blank=True, null=True)
-    event_code = models.TextField(blank=True)
-    new_state_code = models.TextField(blank=True)
-    test_result_code = models.TextField(blank=True)
+    event_code = models.CharField(max_length=255, blank=True)
+    new_state_code = models.CharField(max_length=255, blank=True)
+    test_result_code = models.CharField(max_length=255, blank=True)
+
 
 class Outdailyexposures(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
@@ -200,10 +194,11 @@ class Outdailyexposures(models.Model):
     exposed_zone_id = models.IntegerField(blank=True, null=True)
     exposing_herd_id = models.IntegerField(blank=True, null=True)
     exposing_zone_id = models.IntegerField(blank=True, null=True)
-    spread_method_code = models.TextField(blank=True)
+    spread_method_code = models.CharField(max_length=255, blank=True)
     is_adequate = models.IntegerField(blank=True, null=True)
-    exposing_herd_status_code = models.TextField(blank=True)
-    exposed_herd_status_code = models.TextField(blank=True)
+    exposing_herd_status_code = models.CharField(max_length=255, blank=True)
+    exposed_herd_status_code = models.CharField(max_length=255, blank=True)
+
 
 class Outepidemiccurves(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
@@ -216,12 +211,14 @@ class Outepidemiccurves(models.Model):
     infectious_units = models.IntegerField(blank=True, null=True)
     apparent_infectious_units = models.IntegerField(blank=True, null=True)
 
+
 class Outgeneral(models.Model):
-    out_general_id = models.TextField(blank=True)
-    simulation_start_time = models.TextField(blank=True)
-    simulation_end_time = models.TextField(blank=True)
+    out_general_id = models.CharField(max_length=255, blank=True)
+    simulation_start_time = models.CharField(max_length=255, blank=True)
+    simulation_end_time = models.CharField(max_length=255, blank=True)
     completed_iterations = models.IntegerField(blank=True, null=True)
-    version = models.TextField(blank=True)
+    version = models.CharField(max_length=255, blank=True)
+
 
 class Outiteration(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
@@ -243,13 +240,15 @@ class Outiteration(models.Model):
     vac_wait_unit_time_max = models.IntegerField(blank=True, null=True)
     vac_wait_unit_time_avg = models.FloatField(blank=True, null=True)
 
+
 class Outiterationbyherd(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
     herd_id = models.IntegerField(blank=True, null=True)
-    last_status_code = models.TextField(blank=True)
+    last_status_code = models.CharField(max_length=255, blank=True)
     last_status_day = models.IntegerField(blank=True, null=True)
-    last_control_state_code = models.TextField(blank=True)
+    last_control_state_code = models.CharField(max_length=255, blank=True)
     last_control_state_day = models.IntegerField(blank=True, null=True)
+
 
 class Outiterationbyproductiontype(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
@@ -362,6 +361,7 @@ class Outiterationbyproductiontype(models.Model):
     first_vaccination = models.IntegerField(blank=True, null=True)
     last_detection = models.IntegerField(blank=True, null=True)
 
+
 class Outiterationbyzone(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
     zone_id = models.IntegerField(blank=True, null=True)
@@ -372,6 +372,7 @@ class Outiterationbyzone(models.Model):
     max_zone_perimeter_day = models.IntegerField(blank=True, null=True)
     final_zone_perimeter = models.FloatField(blank=True, null=True)
 
+
 class Outiterationbyzoneandproductiontype(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
     zone_id = models.IntegerField(blank=True, null=True)
@@ -379,6 +380,7 @@ class Outiterationbyzoneandproductiontype(models.Model):
     unit_days_in_zone = models.IntegerField(blank=True, null=True)
     animal_days_in_zone = models.IntegerField(blank=True, null=True)
     cost_surveillance = models.FloatField(blank=True, null=True)
+
 
 class Outiterationcosts(models.Model):
     iteration = models.IntegerField(blank=True, null=True)
