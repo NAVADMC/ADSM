d1871e2cd62eb6eed85c19df5821c12a77085b5f
josiahseaman <josiah.seaman@gmail.com>
2014-03-05 17:16:30 -0700

    Add button creates empty modal.

---
+++ SpreadModel/static/js/spreadmodel.js
@@ -0,0 +1,34 @@
+$(function(){
+    $('.btn-add').on('click', function(){
+        var $selector = $($(this).attr('rel'));
+        var model = $selector.attr('name'); // field name
+        modelModal.show(model, $selector);
+    })
+})
+
+var modelModal = {
+    show: function(model, selectInput) {
+        var modal = this.template.clone();
+        modal.attr('id', model+'_modal');
+        // do stuff to modal
+        $('body').append(modal)
+        modal.modal('show')
+        },
+    template: $('<div class="modal fade">\
+                  <div class="modal-dialog">\
+                    <div class="modal-content">\
+                      <div class="modal-header">\
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
+                        <h4 class="modal-title">Modal title</h4>\
+                      </div>\
+                      <div class="modal-body">\
+                        <p>One fine body&hellip;</p>\
+                      </div>\
+                      <div class="modal-footer">\
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+                        <button type="button" class="btn btn-primary">Save changes</button>\
+                      </div>\
+                    </div>\
+                  </div>\
+                </div>')
+}
