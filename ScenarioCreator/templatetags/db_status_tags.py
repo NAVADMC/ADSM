a94513988e8633030b15a36518eb30fcc4241714
josiahseaman <josiah.seaman@gmail.com>
2014-03-19 17:41:23 -0600

    changed completed to a filer.

--- ScenarioCreator/templatetags/db_status_tags.py
+++ ScenarioCreator/templatetags/db_status_tags.py
@@ -6,12 +6,21 @@ from django.template.base import Node
 register = template.Library()
 
 
+@register.filter()
+def completed(queryset):
+    print(queryset)
+    if queryset:
+        return 'completed '
+    return ''
+
+
 class FormCompleted(Node):
     def __init__(self, variable):
         self.queryset = variable
 
     def render(self, context):
-        if len(self.queryset) > 0:
+        print(self.queryset)
+        if self.queryset:
             return 'completed '
         return ''
 
@@ -22,4 +31,4 @@ def form_completed(parser, token):
         tag_name, queryset = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
-    return FormCompleted(queryset[1:-1])
+    return FormCompleted(queryset)
