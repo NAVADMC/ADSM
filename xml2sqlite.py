f9937858c852b2d569c46d0729cfd582b795b9a3
ndh2 <ndh2@users.noreply.github.com>
2014-04-09 08:14:57 -0400

    Now reads disease-model and airborne-spread-[exponential-]model elements from the XML. Supports only "Point" PDFs so far.

--- xml2sqlite.py
+++ xml2sqlite.py
@@ -16,24 +16,56 @@ import xml.etree.ElementTree as ET
 
 
 
+def getProductionTypes( text, allowedNames ):
+	if text == '':
+		types = allowedNames
+	else:
+		types = text.split(',')
+	return types
+
+
+
+def getPdf( xml ):
+	"""Returns a ProbabilityFunction object corresponding to the XML."""
+	assert isinstance( xml, ET.Element )
+	firstChild = list( xml )[0]
+	if firstChild.tag == 'probability-density-function':
+		# New style
+		firstChild = list( firstChild )[0]
+		# Now "firstChild" is the PDF element
+	pdfName = firstChild.tag
+
+	args = {'equation_type': pdfName.capitalize()}
+
+	if pdfName == 'point':
+		args['mode'] = float( firstChild.text )
+	else:
+		print( pdfName )
+		raise NotImplementedError
+
+	return ProbabilityFunction( **args )
+
+
+
 def main():
 	# Make sure the database has all the correct tables.
 	call_command('syncdb', verbosity=0)
 
 	xml = ET.parse( sys.stdin ).getroot()
 
+	useAirborneExponentialDecay = (xml.find( './/airborne-spread-exponential-model' ) != None)
 	scenario = Scenario(
 	  description = xml.find( './description' ).text,
 	  include_airborne_spread = (xml.find( './/airborne-spread-model' ) != None or xml.find( './/airborne-spread-exponential-model' ) != None),
-	  use_airborne_exponential_decay = (xml.find( './/airborne-spread-exponential-model' ) != None),
+	  use_airborne_exponential_decay = useAirborneExponentialDecay,
 	  use_within_unit_prevalence = (xml.find( './/disease-model/prevalence' ) != None)
 	)
 	scenario.save()
 
 	if xml.find( './/state-table-writer' ) != None:
-		statesFile = '"-"'
+		statesFile = '-'
 	else:
-		statesFile = 'NULL'
+		statesFile = None
 	outputSettings = OutputSettings(
       iterations = int( xml.find( './num-runs' ).text ),
       days = int( xml.find( './num-days' ).text ),
@@ -42,18 +74,82 @@ def main():
 	outputSettings.save()
 
 	# Gather the production type names into a set.
-	names = set( [el.attrib['production-type'] for el in xml.findall( './/*[@production-type]' )] )
-	names.update( set( [el.attrib['to-production-type'] for el in xml.findall( './/*[@to-production-type]' )] ) )
-	names.update( set( [el.attrib['from-production-type'] for el in xml.findall( './/*[@from-production-type]' )] ) )
+	productionTypeNames = set( [el.attrib['production-type'] for el in xml.findall( './/*[@production-type]' )] )
+	productionTypeNames.update( set( [el.attrib['to-production-type'] for el in xml.findall( './/*[@to-production-type]' )] ) )
+	productionTypeNames.update( set( [el.attrib['from-production-type'] for el in xml.findall( './/*[@from-production-type]' )] ) )
 	# If an empty production type attribute appeared anywhere in the XML,
 	# ignore that.
-	names.remove( '' )
-	for name in names:
+	productionTypeNames.remove( '' )
+	for name in productionTypeNames:
 		productionType = ProductionType( name=name )
 		productionType.save()
 
 	disease = Disease( name='' )
 	disease.save()
+
+	for el in xml.findall( './/disease-model' ):
+		latentPeriod = getPdf( el.find( './latent-period' ) )
+		subclinicalPeriod = getPdf( el.find( './infectious-subclinical-period' ) )
+		clinicalPeriod = getPdf( el.find( './infectious-clinical-period' ) )
+		immunePeriod = getPdf( el.find( './immunity-period' ) )
+		latentPeriod.save()
+		subclinicalPeriod.save()
+		clinicalPeriod.save()
+		immunePeriod.save()
+		diseaseReaction = DiseaseReaction(
+		  _disease = disease,
+		  disease_latent_period = latentPeriod,
+		  disease_subclinical_period = clinicalPeriod,
+		  disease_clinical_period = subclinicalPeriod,
+		  disease_immune_period = immunePeriod
+		)
+		diseaseReaction.save()
+
+		for productionTypeName in getProductionTypes( el.attrib['production-type'], productionTypeNames ):
+			diseaseReactionAssignment = DiseaseReactionAssignment(
+			  production_type = ProductionType.objects.get( name=productionTypeName ),
+			  reaction = diseaseReaction
+			)
+			diseaseReactionAssignment.save()
+		# end of loop over production types covered by this <disease-model> element
+	# end of loop over <disease-model> elements
+
+	# Create a PDF that always returns 0. Will be used as the default when the
+	# transport delay PDF is missing.
+	zeroDelay = ProbabilityFunction( equation_type='Point', mode=0 )
+	zeroDelay.save()
+
+	for el in xml.findall( './/airborne-spread-model' ) + xml.findall( './/airborne-spread-exponential-model' ):
+		if useAirborneExponentialDecay:
+			maxDistance = 0
+		else:
+			maxDistance = float( el.find( './max-spread/value' ).text )
+		if el.find( './delay' ) != None:
+			delay = getPdf( el.find( './delay' ) )
+			delay.save()
+		else:
+			delay = zeroDelay
+		airborneSpreadModel = AirborneSpreadModel(
+		  _disease = disease,
+		  max_distance = maxDistance,
+		  spread_1km_probability = float( el.find( './prob-spread-1km' ).text ),
+		  wind_direction_start = float( el.find( './wind-direction-start/value' ).text ),
+		  wind_direction_end = float( el.find( './wind-direction-end/value' ).text ),
+		  transport_delay = delay
+		)
+		airborneSpreadModel.save()
+
+		for fromTypeName in getProductionTypes( el.attrib['from-production-type'], productionTypeNames ):
+			for toTypeName in getProductionTypes( el.attrib['to-production-type'], productionTypeNames ):
+				pairing = ProductionTypePairTransmission(
+				  source_production_type = ProductionType.objects.get( name=fromTypeName ),
+				  destination_production_type = ProductionType.objects.get( name=toTypeName ),
+				  airborne_contact_spread_model = airborneSpreadModel
+				)
+				pairing.save()
+			# end of loop over to-production-types covered by this <airborne-spread[-exponential]-model> elements	
+		# end of loop over from-production-types covered by this <airborne-spread[-exponential]-model> elements
+	# end of loop over <airborne-spread[-exponential]-model> elements
 
 
 
