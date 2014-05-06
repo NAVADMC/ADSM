4d90857e4a628cb7a6bc9593b67a3b9a4b4dcca0
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-05-06 15:15:04 -0600

    Trying to differentiate Percentages and failing. #44

--- ScenarioCreator/custom_fields.py
+++ ScenarioCreator/custom_fields.py
@@ -1,30 +1,45 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-__author__ = 'Bryan'
+
+class Percentage(float):
+    """This is a type wrapper for float to make it clear if .3 == .3% or 30%.
+     This tracks whether the /100 conversion has already been done or not"""
+    # def __repr__(self):
+    #     return "Percentage(" + super().__repr__() + ")"
+    #
+    # def __str__(self):
+    #     return "Percentage(" + super().__str__() + ")"
 
 
-class PercentField(models.FloatField):
+class PercentField(models.FloatField, metaclass=models.SubfieldBase):
     """ Float field that ensures field value is in the range 0-100. """
-    default_validators = [
-        MinValueValidator(0),
-        MaxValueValidator(100)]
+    # default_validators = [
+    #     MinValueValidator(0),
+    #     MaxValueValidator(100)]
 
     def to_python(self, value):
-        if not value:
-            return
-        if value > 1:  # why is this here?
+        print("to_python", value)
+        if type(value) == type(Percentage(50.0)):  # check if it's already converted
             return value
-        return value * 100
+        return Percentage(value / 100)
+
 
     def get_db_prep_value(self, value, connection, prepared=False):
+        print("get_db_prep_value", value)
         if not value:
             return
-        # assert (isinstance(value, float))
-        return value / 100
+        # if not isinstance(value, Percentage):
+        #     print( "ValueError: ", value)
+        # assert (isinstance(value, Percentage))
+        return float(value * 100)  # the type wrapper makes it clear if this has already been converted or not
+
+    def value_to_string(self, obj):  # no idea if this has a purpose
+        value = self._get_val_from_obj(obj)
 
 
 class ListField(models.TextField):
+    """Written by Bryan Hurst @newline.us"""
     __metaclass__ = models.SubfieldBase
 
     def __init__(self, token=",", *args, **kwargs):
