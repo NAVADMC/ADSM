c2633d8f4a2dfc1dd9519afc73417a9f0b0113b5
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-06-05 13:17:37 -0600

    Working set widget  #77

--- SpreadModel/static/js/many-to-many-widget.js
+++ SpreadModel/static/js/many-to-many-widget.js
@@ -15,11 +15,12 @@ form_state = (function(form){
     //list Assignments already in DB
     //serialize -> POST
     //get(source destination)
-    var flatten_form_array = function(serialized_array){
+    var format_form_array = function(serialized_array){
         var row = {};
         $.each(serialized_array, function(index, container){
             var name = container.name.replace(/form-\d+-/g, "")
             row[name] = container.value;
+            row['form_number'] = /form-(\d+)-/g.exec(container.name)[1]
         });
         return row;
     };
@@ -30,7 +31,7 @@ form_state = (function(form){
         }
         var matching_row;
         form.find('tbody tr').each(function(){
-            var row_data = flatten_form_array($(this).find(':input').serializeArray());
+            var row_data = format_form_array($(this).find(':input').serializeArray());
             var pass = true;
             $.each( filters, function( key, value ) {
                 if(!row_data.hasOwnProperty(key))
@@ -45,8 +46,19 @@ form_state = (function(form){
         });
         return matching_row
     };
-    //set [sources, destinations]
-    var set = function(){};
+
+    //This will only set the first row that meets the filters criteria.  It will set multiple inputs
+    //that match 'variables' in that row.
+    var set = function(filters, variables){
+        if(typeof filters != "object" || typeof variables != "object"){
+            throw new TypeError("Must be an object containing, (field, value) pairs");
+        }
+        var matching_row_number = get(filters)['form_number'];
+        for(var key in variables){
+            var input_name = 'id_form-' + matching_row_number + '-' + key;
+            $('#'+input_name).val(variables[key]);
+        }
+    };
 
     return {
         'save': function(){$.post('', form.serialize())},
