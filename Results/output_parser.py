a9dac2318e237304a5e885ec4d196abe30adc6ab
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-06-27 11:18:33 -0600

    Successfully parsing and storing exact matches for #131.  Now comes the hard part.

--- Results/output_parser.py
+++ Results/output_parser.py
@@ -1,14 +1,33 @@
-from Results.models import DailyReport
 from ast import literal_eval
+import Results.models
 
 
-# getattr(type(self.instance), field).field.rel.to
-# parentObj._meta.get_field('myField').rel.to
+def save_value_to_proper_field(c_header_name, value, iteration, day):
+    daily_tables = [Results.models.DailyControls(), ]
+        # Results.models.DailyByZoneAndProductionType(), Results.models.DailyByZone(), Results.models.DailyByProductionType()]
+    # DailyByProductionType is listed last because it has the most compositing.
+    for table in daily_tables:
+        for name, obj in table:
+            if str(name) == str(c_header_name):
+                print("==Storing==", name, obj, value)
+                table_instance = type(table).objects.get_or_create(iteration=iteration, day=day)[0]
+                setattr(table_instance, name, value)
+                table_instance.save()
+                return True
+    # getattr(type(self.instance), field).field.rel.to
+    # parentObj._meta.get_field('myField').rel.to
+    return False
 
-def populate_db_from_daily_report(report):
+
+def populate_db_from_daily_report(sparse_info):
     """Parses the C Engine stdout and populates the appropriate models with the information.  Takes one line
     at a time, representing one DailyReport."""
-    assert isinstance(report, DailyReport)
-    sparse_info = literal_eval(report.sparse_dict)
-    for name, value in sparse_info.items():
-        print(name, value)
+    assert isinstance(sparse_info, dict)
+    # sparse_info = literal_eval(report.sparse_dict)
+    print(sparse_info)
+    iteration = sparse_info['Run']
+    del sparse_info['Run']
+    day = sparse_info['Day']
+    del sparse_info['Day']
+    for c_header_name, value in sparse_info.items():
+        save_value_to_proper_field(c_header_name, value, iteration, day)
