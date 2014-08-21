b0d04fc66f00b9a3f86ad5d7138c815f93129b60
Geoffrey Thompson <fleaplus@gmail.com>
2014-08-21 14:35:13 -0600

    created tests module for ScenarioCreator. moved test related fixtures into module.

---
+++ ScenarioCreator/tests/test_parser.py
@@ -0,0 +1,85 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+
+from django.test import TestCase
+
+# Create your tests here.
+from ScenarioCreator.models import Scenario, choice_char_from_value, squish_name, Unit, Population, ProductionType
+from ScenarioCreator.parser import PopulationParser
+
+POPULATION_FIXTURES = 'ScenarioCreator/tests/population_fixtures/'
+
+
+class PopulationParserTestCase(TestCase):
+    def test_parser_load_invalid_file(self):
+        with self.assertRaises(OSError):
+            PopulationParser('Invalid_File.xml')
+
+    def test_parser_load_blank_file(self):
+        with self.assertRaises(EOFError) as e:
+            p = PopulationParser('workspace/Blank.xml')
+
+        self.assertEqual(str(e.exception), "File Read returned a blank string.")
+
+    def test_parser_load_utf8(self):
+        expected_results = {
+            'user_notes': '1',
+            'initial_size': '84',
+            'latitude': '52.9672',
+            'longitude': '-8.201',
+            'production_type': 'B',
+            'initial_state': 'Susceptible',
+        }
+        p = PopulationParser(POPULATION_FIXTURES + 'Population_Test_UTF8.xml')
+
+        results = p.parse_to_dictionary()
+
+        self.assertEqual(len(results), 1)
+        self.assertDictEqual(results[0], expected_results)
+
+    def test_parser_load_utf16(self):
+        expected_results = {
+            'user_notes': '\u5f71\u97ff\u3092\u53d7\u3051\u3084\u3059\u3044',
+            'initial_size': '84',
+            'latitude': '52.9672',
+            'longitude': '-8.201',
+            'production_type': 'B',
+            'initial_state': 'Susceptible',
+        }
+        p = PopulationParser(POPULATION_FIXTURES + 'Population_Test_UTF16.xml')
+
+        results = p.parse_to_dictionary()
+
+        self.assertEqual(len(results), 1)
+        self.assertDictEqual(results[0], expected_results)
+
+    def test_parser_multiple_herds(self):
+        expected_results = [
+            {
+                'user_notes': '1',
+                'initial_size': '84',
+                'latitude': '52.9672',
+                'longitude': '-8.201',
+                'production_type': 'B',
+                'initial_state': 'Susceptible',
+            },
+            {
+                'user_notes': '2',
+                'initial_size': '64',
+                'latitude': '52.9672',
+                'longitude': '-8.21',
+                'production_type': 'B',
+                'initial_state': 'Susceptible',
+            },
+        ]
+
+        p = PopulationParser(POPULATION_FIXTURES + 'Population_Test_Multiple.xml')
+
+        results = p.parse_to_dictionary()
+
+        self.assertEqual(len(results), 2)
+        self.assertDictEqual(results[0], expected_results[0])
+        self.assertDictEqual(results[1], expected_results[1])
--- ScenarioCreator/tests/tests.py
+++ ScenarioCreator/tests/tests.py
@@ -71,76 +71,7 @@ class ModelUtilsTest(TestCase):
         self.assertEqual( Unit.objects.get(id=index+1)._population, p, "New Unit should link back to newest Population object")
 
 
-class PopulationParserTestCase(TestCase):
-    def test_parser_load_invalid_file(self):
-        with self.assertRaises(OSError):
-            PopulationParser('Invalid_File.xml')
 
-    def test_parser_load_blank_file(self):
-        with self.assertRaises(EOFError) as e:
-            p = PopulationParser('workspace/Blank.xml')
-
-        self.assertEqual(str(e.exception), "File Read returned a blank string.")
-
-    def test_parser_load_utf8(self):
-        expected_results = {
-            'user_notes': '1',
-            'initial_size': '84',
-            'latitude': '52.9672',
-            'longitude': '-8.201',
-            'production_type': 'B',
-            'initial_state': 'Susceptible',
-        }
-        p = PopulationParser('workspace/Population_Test_UTF8.xml')
-
-        results = p.parse_to_dictionary()
-
-        self.assertEqual(len(results), 1)
-        self.assertDictEqual(results[0], expected_results)
-
-    def test_parser_load_utf16(self):
-        expected_results = {
-            'user_notes': '\u5f71\u97ff\u3092\u53d7\u3051\u3084\u3059\u3044',
-            'initial_size': '84',
-            'latitude': '52.9672',
-            'longitude': '-8.201',
-            'production_type': 'B',
-            'initial_state': 'Susceptible',
-        }
-        p = PopulationParser('workspace/Population_Test_UTF16.xml')
-
-        results = p.parse_to_dictionary()
-
-        self.assertEqual(len(results), 1)
-        self.assertDictEqual(results[0], expected_results)
-
-    def test_parser_multiple_herds(self):
-        expected_results = [
-            {
-                'user_notes': '1',
-                'initial_size': '84',
-                'latitude': '52.9672',
-                'longitude': '-8.201',
-                'production_type': 'B',
-                'initial_state': 'Susceptible',
-            },
-            {
-                'user_notes': '2',
-                'initial_size': '64',
-                'latitude': '52.9672',
-                'longitude': '-8.21',
-                'production_type': 'B',
-                'initial_state': 'Susceptible',
-            },
-        ]
-
-        p = PopulationParser('workspace/Population_Test_Multiple.xml')
-
-        results = p.parse_to_dictionary()
-
-        self.assertEqual(len(results), 2)
-        self.assertDictEqual(results[0], expected_results[0])
-        self.assertDictEqual(results[1], expected_results[1])
 
 
 class CleanTest(unittest.TestCase):
