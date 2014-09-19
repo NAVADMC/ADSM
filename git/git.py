0dfe39b5bde61dce2862435a92dd7f3ce53ffce1
Bryan <bryan@newlinetechnicalinnovations.com>
2014-09-19 08:05:52 -0600

    Fix git path

--- git/git.py
+++ git/git.py
@@ -3,4 +3,4 @@ import os
 __dir = os.path.dirname(os.path.realpath(__file__))
 
 # TODO: Change this for os specific stuff
-git = os.path.join(__dir, 'git', 'bin', 'git.exe')
+git = os.path.join(__dir, 'bin', 'git.exe')
