b899b5ba5d8dccc0059c6df5432dea41973e55f0
Josiah Seaman <josiah.seaman@gmail.com>
2014-09-24 14:31:18 -0600

    Fixed unicode_literals battling with Windows

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
@@ -14,11 +13,6 @@ def main(*args):
     execute_from_command_line(*args)
 
 if __name__ == "__main__":
-    key = "DJANGO_SETTINGS_MODULE"
-    value = "SpreadModel.settings"
-    if os.name == 'nt':
-        key = key.encode('ascii', 'ignore')
-        value = value.encode('ascii', 'ignore')
-    os.environ.setdefault(key, value)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpreadModel.settings')
 
     main(sys.argv)
