1af2cc3f61ef6f8f99c6ec558213bb07c56b30ef
Geoffrey Thompson <fleaplus@gmail.com>
2014-10-09 15:44:01 -0600

    added tests for RelationalFunction view. changed initialize_points_from_csv to use the file that it just copied into the workspace, and properly delete afterwards. Fixes #207

--- ScenarioCreator/tests/test_views.py
+++ ScenarioCreator/tests/test_views.py
@@ -7,10 +7,12 @@ standard_library.install_hooks()
 import unittest
 import os
 import json
+import tempfile
 
 from django.test import TestCase
 
-from ScenarioCreator.models import AirborneSpread
+from ScenarioCreator.models import (AirborneSpread, RelationalFunction,
+        RelationalPoint, ProbabilityFunction, DiseaseProgression)
 
 POPULATION_FIXTURES = 'ScenarioCreator/tests/population_fixtures/'
 
@@ -60,3 +62,152 @@ class PopulationTestCase(TestCase):
 
         data = json.loads(r.content)
         self.assertEqual(data, expected_results)
+
+class RelationalFunctionTestCase(TestCase):
+    multi_db = True
+
+    def test_list(self):
+        r = self.client.get('/setup/Function/')
+
+        self.assertEqual(r.status_code, 200)
+        self.assertIn("Create Functions", r.content)
+
+    def test_get(self):
+        function = RelationalFunction.objects.create(name="Test Function")
+
+        r = self.client.get('/setup/RelationalFunction/%d/' % function.pk)
+
+        self.assertEqual(r.status_code, 200)
+        self.assertIn('title', r.context)
+        self.assertEqual('Create a Relational Function', r.context['title'])
+        self.assertIn('Test Function', r.content)
+
+    def test_post_no_points(self):
+        form_data = {
+            'relationalpoint_set-TOTAL_FORMS': '0',
+            'relationalpoint_set-INITIAL_FORMS': '0',
+            'relationalpoint_set-MAX_NUM_FORMS': '1',
+            'name': 'Test Function',
+            'x_axis_units': 'Days',
+        }
+        r = self.client.post('/setup/RelationalFunction/new/', form_data, follow=True)
+
+        self.assertEqual(RelationalFunction.objects.count(), 1)
+        function = RelationalFunction.objects.first()
+        self.assertEqual(function.name, 'Test Function')
+        self.assertRedirects(r, '/setup/RelationalFunction/%d/' % function.pk)
+
+    def test_post_with_points(self):
+        form_data = {
+            'relationalpoint_set-TOTAL_FORMS': '1',
+            'relationalpoint_set-INITIAL_FORMS': '0',
+            'relationalpoint_set-MAX_NUM_FORMS': '1',
+            'relationalpoint_set-0-x': '0.0',
+            'relationalpoint_set-0-y': '1.0',
+            'name': 'Test Function',
+            'x_axis_units': 'Days',
+        }
+        r = self.client.post('/setup/RelationalFunction/new/', form_data, follow=True)
+
+        self.assertEqual(RelationalFunction.objects.count(), 1)
+        function = RelationalFunction.objects.first()
+        self.assertEqual(RelationalPoint.objects.count(), 1)
+        point = RelationalPoint.objects.first()
+        self.assertEqual(point.relational_function, function)
+        self.assertEqual(point.x, 0.0)
+        self.assertEqual(point.y, 1.0)
+        self.assertEqual(function.name, 'Test Function')
+        self.assertRedirects(r, '/setup/RelationalFunction/%d/' % function.pk)
+
+    def test_copy_no_points(self):
+        form_data = {
+            'relationalpoint_set-TOTAL_FORMS': '0',
+            'relationalpoint_set-INITIAL_FORMS': '0',
+            'relationalpoint_set-MAX_NUM_FORMS': '1',
+            'name': 'Test Function',
+            'x_axis_units': 'Days',
+        }
+        function = RelationalFunction.objects.create(name="Test Function")
+
+        r = self.client.post('/setup/RelationalFunction/%d/copy/' % function.pk, form_data)
+
+        self.assertEqual(RelationalFunction.objects.count(), 2)
+        qs = RelationalFunction.objects.filter(name='Test Function')
+        qs = qs.exclude(pk=function.pk)
+        self.assertEqual(qs.count(), 1)
+        new_function = qs.first()
+
+        self.assertRedirects(r, '/setup/RelationalFunction/%d/' % new_function.pk)
+
+    def test_copy_with_points(self):
+        form_data = {
+            'relationalpoint_set-TOTAL_FORMS': '1',
+            'relationalpoint_set-INITIAL_FORMS': '0',
+            'relationalpoint_set-MAX_NUM_FORMS': '1',
+            'relationalpoint_set-0-x': '0.0',
+            'relationalpoint_set-0-y': '1.0',
+            'name': 'Test Function',
+            'x_axis_units': 'Days',
+        }
+        function = RelationalFunction.objects.create(name="Test Function")
+        point = RelationalPoint.objects.create(x=0.0, y=1.0, relational_function=function)
+
+        r = self.client.post('/setup/RelationalFunction/%d/copy/' % function.pk, form_data)
+
+        self.assertEqual(RelationalFunction.objects.count(), 2)
+        qs = RelationalFunction.objects.filter(name='Test Function')
+        qs = qs.exclude(pk=function.pk)
+        self.assertEqual(qs.count(), 1)
+        new_function = qs.first()
+
+        self.assertEqual(RelationalPoint.objects.count(), 2)
+        new_point = RelationalPoint.objects.get(relational_function=new_function)
+        self.assertNotEqual(new_point.pk, point.pk)
+        self.assertEqual(new_point.x, point.x)
+        self.assertEqual(new_point.y, point.y)
+
+        self.assertRedirects(r, '/setup/RelationalFunction/%d/' % new_function.pk)
+
+    def test_post_with_points_from_file(self):
+        points_file = tempfile.NamedTemporaryFile(delete=False)
+        points_file.write("x,y\n0.0, 1.0\n")
+        points_file.seek(0)
+        form_data = {
+            'relationalpoint_set-TOTAL_FORMS': '0',
+            'relationalpoint_set-INITIAL_FORMS': '0',
+            'relationalpoint_set-MAX_NUM_FORMS': '1',
+            'name': 'Test Function',
+            'x_axis_units': 'Days',
+            'file': points_file
+        }
+        r = self.client.post('/setup/RelationalFunction/new/', form_data, follow=True)
+
+        self.assertEqual(RelationalFunction.objects.count(), 1)
+        function = RelationalFunction.objects.first()
+        self.assertEqual(function.name, 'Test Function')
+        self.assertEqual(RelationalPoint.objects.count(), 1)
+        point = RelationalPoint.objects.first()
+        self.assertEqual(point.x, 0.0)
+        self.assertEqual(point.y, 1.0)
+        self.assertRedirects(r, '/setup/RelationalFunction/%d/' % function.pk)
+
+        points_file.close()
+        os.unlink(points_file.name)
+
+    def test_related_objects_exist(self):
+        relational_function = RelationalFunction.objects.create(name="Test Function")
+        probability_function = ProbabilityFunction.objects.create(name="Test Probability Function")
+        disease_progression = DiseaseProgression.objects.create(
+            name="Test Progression",
+            disease_latent_period=probability_function,
+            disease_subclinical_period=probability_function,
+            disease_clinical_period=probability_function,
+            disease_immune_period=probability_function,
+            disease_prevalence=relational_function)
+        progression_url = '/setup/DiseaseProgression/%d/' % disease_progression.pk
+
+        r = self.client.get('/setup/RelationalFunction/%d/' % relational_function.pk)
+
+        self.assertIn('backlinks', r.context)
+        self.assertIn('Test Progression', r.context['backlinks'])
+        self.assertEqual(progression_url, r.context['backlinks']['Test Progression'])
