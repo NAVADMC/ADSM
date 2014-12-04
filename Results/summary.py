e3c3c87773148f5c14dbabdb833adc3d0332b582
Josiah Seaman <josiah.seaman@gmail.com>
2014-12-04 16:50:18 -0700

    Better Output Summary

--- Results/summary.py
+++ Results/summary.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from Results.models import DailyControls, DailyByProductionType, DailyByZone
+from ScenarioCreator.models import Zone
 from ScenarioCreator.models import Zone, OutputSettings
 from django.db.models import Q
 
@@ -11,9 +12,10 @@ def list_of_iterations():
 def median_value(queryset, term):
     count = queryset.count()
     try:
-        return queryset.values_list(term, flat=True).order_by(term)[int(round(count/2))]
+        med = queryset.values_list(term, flat=True).order_by(term)[int(round(count / 2))]
+        return med if med != -1 else "N/A"
     except:
-        return 0
+        return "N/A"
 
 
 def last_day_query(model=DailyControls):
@@ -41,25 +43,33 @@ def name_and_value(field_name, model=DailyByProductionType):
     return name(field_name, model), field_summary(field_name, model)
 
 
-def pair(unit_pair, animal_number):
-    return unit_pair[0], "%s (%s)" % (str(unit_pair[1]), str(animal_number))
+def pair(unit_number, animal_number):
+    return "%s (%s)" % (str(unit_number), str(animal_number))
 
 
 def summarize_results():
     summary = OrderedDict()
     summary["Unit (Animal) Summary"] = [
-        pair(name_and_value("infcU"), field_summary("infcA")),
-        pair(name_and_value("firstDetUInf", DailyControls), field_summary("firstDetAInf", DailyControls)),
-        pair(name_and_value("descU"), field_summary("descA")),
-        pair(name_and_value("vaccU"), field_summary("vaccA"))]
-    summary["Days Summary"] = [
-        name_and_value("outbreakDuration", DailyControls),
-        name_and_value("diseaseDuration", DailyControls),
-        name_and_value("firstDetection"),
-        name_and_value("firstVaccination"),
-        name_and_value("firstDestruction"),
-        name_and_value("zoneArea", DailyByZone)]
-
+        ("Median Infected Units (Animals)", pair(field_summary("infcU"), field_summary("infcA"))),
+        ("Median Units (Animals) Infected at First Detection", pair(field_summary("firstDetUInf", DailyControls), field_summary("firstDetAInf", DailyControls))),
+        ("Median Depopulated Units (Animals)", pair(field_summary("descU"), field_summary("descA"))),
+        ("Median Vaccinated Units (Animals)", pair(field_summary("vaccU"), field_summary("vaccA")))]
+    summary["Event Summary"] = [
+        ("Median Outbreak Duration in Days (end of control activities)", 
+            field_summary("outbreakDuration", DailyControls)),
+        ("Median Duration of Disease Spread in Days",field_summary("diseaseDuration", DailyControls)),
+        ("Median Day of First Detection", field_summary("firstDetection")),
+        ("Median Day of First Vaccination", field_summary("firstVaccination")),
+        ("Median Day of First Destruction", field_summary("firstDestruction"))]
+    zones_summary = []
+    for zone in Zone.objects.all():
+        queryset = DailyByZone.objects.filter(last_day_query(), zone=zone)
+        zones_summary.append(
+            ("Median Total Area of %s in km^2" % zone.name, median_value(queryset, "zoneArea")))
+        zones_summary.append(
+            ("Median Number of Distinct %s Zones" % zone.name, median_value(queryset, "numSeparateAreas")))
+    summary["Zone Summary"] = zones_summary
+    
     return summary
 
 
