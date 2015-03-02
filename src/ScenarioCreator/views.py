8bf6d0ddc16dd218bda0434be56308f0385c12f1
Josiah Seaman <josiah.seaman@gmail.com>
2015-03-02 14:48:31 -0700

    #285 csv examples and new validation error: "There are no infected units in the Population.  Please set at least one unit to 'Latent' in the Population screen."

--- src/ScenarioCreator/views.py
+++ src/ScenarioCreator/views.py
@@ -470,16 +470,18 @@ def population(request):
 def whole_scenario_validation():
     fatal_errors = []
     for pt in ProductionType.objects.all():
-        count = DiseaseProgressionAssignment.objects.filter(production_type=pt, progression__isnull=False).count()
-        if not count:
+        if not DiseaseProgressionAssignment.objects.filter(production_type=pt, progression__isnull=False).count():
             fatal_errors.append(pt.name + " has no Disease Progression assigned and so is a non-participant in the simulation.")
-        count = DiseaseSpreadAssignment.objects.filter(source_production_type=pt, 
-                                                              destination_production_type=pt,
-                                                              direct_contact_spread__isnull=False).count()
-        if not count:
-            fatal_errors.append(pt.name + " cannot spread disease from one animal to another, because Direct Spread %s -> %s has not been assigned." % (pt.name, pt.name))
+        if not DiseaseSpreadAssignment.objects.filter(source_production_type=pt,
+                                                      destination_production_type=pt,
+                                                      direct_contact_spread__isnull=False).count():
+            fatal_errors.append(
+                pt.name + " cannot spread disease from one animal to another, because Direct Spread %s -> %s has not been assigned." % (pt.name, pt.name))
     if not Disease.objects.get_or_create()[0].include_direct_contact_spread:
         fatal_errors.append("Direct Spread is not enabled in this Simulation.")
+    if not Unit.objects.filter(~Q(initial_state='S')).count():
+        fatal_errors.append("There are no infected units in the Population.  Please set at least one unit to 'Latent' in the Population screen.")
+
     return fatal_errors
 
 
@@ -494,6 +496,6 @@ def validate_scenario(request):
     simulation.wait()  # simulation will process db then exit
     print("Return code", simulation.returncode)
     context = {'dry_run_passed': simulation.returncode == 0 and not stderr,
-               'sim_output': stdout + stderr,
+               'sim_output': stdout.decode() + stderr.decode(),
                'fatal_errors': fatal_errors}
     return render(request, 'ScenarioCreator/Validation.html', context)
