494ca3205cc2f722212f589ed6751f1fc12e1e42
Josiah Seaman <josiah.seaman@gmail.com>
2015-03-10 12:51:08 -0600

    fixed bizarre data migration problem

--- src/ScenarioCreator/migrations/0008_move_vacc_units_detected.py
+++ src/ScenarioCreator/migrations/0008_move_vacc_units_detected.py
@@ -11,8 +11,9 @@ def move_to_new_vaccination_triggers(apps, schema_editor):
 
     try:
         if ControlMasterPlan.objects.count():
-            number_of_units = ControlMasterPlan.objects.get_or_create(id=1).units_detected_before_triggering_vaccination
-            if number_of_units:
+            cm = ControlMasterPlan.objects.get_or_create(id=1)[0]
+            number_of_units = cm.units_detected_before_triggering_vaccination
+            if number_of_units and number_of_units != 'units_detected_before_triggering_vaccination':  # somehow, the value of this gets set to the field name in tests...
                 n = DiseaseDetection.objects.create(number_of_units=int(number_of_units),)
                 n.trigger_group.add(*ProductionType.objects.all())  # triggered on all production types 
                 n.save()
