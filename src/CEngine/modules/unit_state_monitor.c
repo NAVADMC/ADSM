NAVADMC/ADSM

commit 92d49eb12e765a0a49344b23ae818974022f5f72

Removed the "average prevalence" output variable from unit state monitor. #466

src/CEngine/modules/unit_state_monitor.c
@@ -24,7 +24,6 @@
 #define handle_before_any_simulations_event unit_state_monitor_handle_before_any_simulations_event
 #define handle_before_each_simulation_event unit_state_monitor_handle_before_each_simulation_event
 #define handle_unit_state_change_event unit_state_monitor_handle_unit_state_change_event
-#define handle_new_day_event unit_state_monitor_handle_new_day_event
 
 #include "module.h"
 
@@ -61,7 +60,6 @@ typedef struct
   RPT_reporting_t ***num_units_in_state_by_prodtype;
   RPT_reporting_t  **num_animals_in_state;
   RPT_reporting_t ***num_animals_in_state_by_prodtype;
-  RPT_reporting_t   *avg_prevalence;
   RPT_reporting_t   *last_day_of_disease;
   gboolean disease_end_recorded;
   GPtrArray *daily_outputs; /**< Daily outputs, in a list to make it easy to
@@ -218,97 +216,6 @@ handle_unit_state_change_event (struct adsm_module_t_ *self,
 
 
 /**
- * A struct for use with the callback function calculate_prevalence below.
- */
-typedef struct
-{
-  UNT_unit_list_t *units;
-  double prevalence_num;
-  double prevalence_denom;
-}
-calculate_prevalence_args_t;
-
-
-
-/**
- * @param key index of a unit in the unit list.
- * @param value not used.
- * @param user_data a calculate_prevalence_args_t structure.
- */
-static void
-calculate_prevalence (gpointer key, gpointer value, gpointer user_data)
-{
-  calculate_prevalence_args_t *args;
-  UNT_unit_t *unit;
-  double nanimals;
-
-  args = (calculate_prevalence_args_t *) user_data; 
-  unit = UNT_unit_list_get (args->units, GPOINTER_TO_UINT(key));
-  nanimals = (double)(unit->size);
-  args->prevalence_num += nanimals * unit->prevalence;
-  args->prevalence_denom += nanimals;
-
-  return;
-}
-
-
-
-/**
- * Responds to a New Day event by updating the average prevalence.
- *
- * @param self this module.
- * @param event a New Day event.
- * @param units a list of units.
- */
-void
-handle_new_day_event (struct adsm_module_t_ *self,
-                      EVT_new_day_event_t *event,
-                      UNT_unit_list_t *units)
-{
-  local_data_t *local_data;
-  calculate_prevalence_args_t calculate_prevalence_args;
-  gboolean active_infections;
-
-  #if DEBUG
-    g_debug ("----- ENTER handle_new_day_event (%s)", MODEL_NAME);
-  #endif
-
-  local_data = (local_data_t *) (self->model_data);
-
-  /* The _iteration.infectious_units table contains all units that are Latent,
-   * Infectious Subclinical, or Infectious Clinical. */
-  active_infections = (g_hash_table_size(_iteration.infectious_units) > 0);
-  if (active_infections)
-    {
-      local_data->disease_end_recorded = FALSE;
-      /* Calculate the prevalence. */
-      calculate_prevalence_args.units = units;
-      calculate_prevalence_args.prevalence_num = 0;
-      calculate_prevalence_args.prevalence_denom = 0;
-      g_hash_table_foreach (_iteration.infectious_units, calculate_prevalence, &calculate_prevalence_args);
-      RPT_reporting_set_real (local_data->avg_prevalence,
-                              calculate_prevalence_args.prevalence_num / calculate_prevalence_args.prevalence_denom);
-    }
-  else
-    {
-      if (local_data->disease_end_recorded == FALSE)
-        {
-          RPT_reporting_set_integer (local_data->last_day_of_disease, event->day - 1);
-          local_data->disease_end_recorded = TRUE;
-        }
-      RPT_reporting_set_real (local_data->avg_prevalence, 0);
-    }
-
-  #if DEBUG
-    g_debug ("----- EXIT handle_new_day_event (%s)", MODEL_NAME);
-  #endif
-
-  return;
-}
-
-
-
-/**
  * Runs this model.
  *
  * @param self the model.
@@ -336,9 +243,6 @@ run (struct adsm_module_t_ *self, UNT_unit_list_t * units, ZON_zone_list_t * zon
       break;
     case EVT_UnitStateChange:
       handle_unit_state_change_event (self, &(event->u.unit_state_change));
-      break;
-    case EVT_NewDay:
-      handle_new_day_event (self, &(event->u.new_day), units);
       break;
     default:
       g_error
@@ -409,7 +313,6 @@ new (sqlite3 * params, UNT_unit_list_t * units, projPJ projection,
     EVT_BeforeAnySimulations,
     EVT_BeforeEachSimulation,
     EVT_UnitStateChange,
-    EVT_NewDay,
     0
   };
   guint nprodtypes;
@@ -460,11 +363,6 @@ new (sqlite3 * params, UNT_unit_list_t * units, projPJ projection,
         RPT_CharArray, UNT_state_name, UNT_NSTATES,
         RPT_GPtrArray, local_data->production_types, nprodtypes,
         self->outputs, local_data->daily_outputs },
-
-      { &local_data->avg_prevalence, "averagePrevalence", RPT_real,
-        RPT_NoSubcategory, NULL, 0,
-        RPT_NoSubcategory, NULL, 0,
-        self->outputs, NULL },
 
       { &local_data->last_day_of_disease, "diseaseDuration", RPT_integer,
         RPT_NoSubcategory, NULL, 0,

commit 4b982bf8e049f48eec717a478702923367d177d3

Restored the diseaseDuration output variable that I broke in commit 92d49eb

src/CEngine/modules/unit_state_monitor.c
@@ -24,6 +24,7 @@
 #define handle_before_any_simulations_event unit_state_monitor_handle_before_any_simulations_event
 #define handle_before_each_simulation_event unit_state_monitor_handle_before_each_simulation_event
 #define handle_unit_state_change_event unit_state_monitor_handle_unit_state_change_event
+#define handle_new_day_event unit_state_monitor_handle_new_day_event
 
 #include "module.h"
 
@@ -216,6 +217,51 @@ handle_unit_state_change_event (struct adsm_module_t_ *self,
 
 
 /**
+ * Responds to a New Day event by updating the disease duration output.
+ *
+ * @param self this module.
+ * @param event a New Day event.
+ * @param units a list of units.
+ */
+void
+handle_new_day_event (struct adsm_module_t_ *self,
+                      EVT_new_day_event_t *event,
+                      UNT_unit_list_t *units)
+{
+  local_data_t *local_data;
+  gboolean active_infections;
+
+  #if DEBUG
+    g_debug ("----- ENTER handle_new_day_event (%s)", MODEL_NAME);
+  #endif
+
+  local_data = (local_data_t *) (self->model_data);
+
+  /* The _iteration.infectious_units table contains all units that are Latent,
+   * Infectious Subclinical, or Infectious Clinical. */
+  active_infections = (g_hash_table_size(_iteration.infectious_units) > 0);
+  if (active_infections)
+    {
+      local_data->disease_end_recorded = FALSE;
+    }
+  else
+    {
+      if (local_data->disease_end_recorded == FALSE)
+        {
+          RPT_reporting_set_integer (local_data->last_day_of_disease, event->day - 1);
+          local_data->disease_end_recorded = TRUE;
+        }
+    }
+
+  #if DEBUG
+    g_debug ("----- EXIT handle_new_day_event (%s)", MODEL_NAME);
+  #endif
+
+  return;
+}
+
+
+/**
  * Runs this model.
  *
  * @param self the model.
@@ -243,6 +289,9 @@ run (struct adsm_module_t_ *self, UNT_unit_list_t * units, ZON_zone_list_t * zon
       break;
     case EVT_UnitStateChange:
       handle_unit_state_change_event (self, &(event->u.unit_state_change));
+      break;
+    case EVT_NewDay:
+      handle_new_day_event (self, &(event->u.new_day), units);
       break;
     default:
       g_error
@@ -313,6 +362,7 @@ new (sqlite3 * params, UNT_unit_list_t * units, projPJ projection,
     EVT_BeforeAnySimulations,
     EVT_BeforeEachSimulation,
     EVT_UnitStateChange,
+    EVT_NewDay,
     0
   };
   guint nprodtypes;