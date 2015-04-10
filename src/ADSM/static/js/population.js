7c32599e2c7a0e0b0391350b84ae8c3b18749a18
tjmahlin <tjm@tjmahlin.com>
2015-04-10 13:47:46 -0600

    UI adjustments for population interface after merge #333

--- src/ADSM/static/js/population.js
+++ src/ADSM/static/js/population.js
@@ -211,5 +211,9 @@ $(document).on('click', '#refresh_map', function(){
 
 $(document).on('click', '#edit_population', function(){
     $('#population_grid_wraper .buttonHolder').removeAttr('hidden')
+    $('#population_grid_wraper').css('height', '75vh')
     $('#edit-mask').css('visibility', 'visible')
+    $('#farm_list tbody').css('height', 'calc(75vh - 90px')
+    $('#edit_population').css('display', 'none')
+    $('#farm_list select, #farm_list input').addClass('editable')
 })
