63a000a9cb62e232ba9fb61d144bb8920a3470d3
Josiah Seaman <josiah.seaman@gmail.com>
2015-05-29 12:56:20 -0600

    #515 ADSMSettings tests all fixed

--- src/ADSMSettings/tests.py
+++ src/ADSMSettings/tests.py
@@ -33,20 +33,6 @@ class ScenarioTestCase(TestCase):
             pass
         #     self.remove_test_file(file_path)
 
-    def test_post_failure(self):
-        file_name = 'Test \/ Scenario 123 AZ' # this should break Windows and Linux
-        file_path = workspace_path(file_name) + '.sqlite3'
-
-        self.remove_test_file(file_path)
-
-        r = self.client.post('/app/SaveScenario/', {'filename': file_name})
-
-        try:
-            self.assertEqual(r.status_code, 200)
-            self.assertFalse(os.path.isfile(file_path))
-        finally:
-            self.remove_test_file(file_path)
-
     def test_post_failure_ajax(self):
         file_name = 'Test \/ Scenario 123 AZ' # this should break Windows and Linux
         file_path = workspace_path(file_name) + '.sqlite3'
