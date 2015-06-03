82da2c53289fa13336e1868c49cfba96de1375c7
Bryan Hurst <bryan@newline.us>
2015-06-03 10:33:38 -0600

    Windows Staging Compile 06/03/2015 @ 10:33

--- src/static/js/control-protocol.js
+++ src/static/js/control-protocol.js
@@ -4,7 +4,7 @@ $(function(){
     var check_enabled_tabs = function(){
         $('.tab-pane').each(function(){
             var use_check = $(this).find(':checkbox').first();
-            if(use_check.length > 0 && use_check[0].id.indexOf('use_') != -1) { // one of the use_detection, use_tracing check boxes
+            if(use_check.length > 0 && use_check[0].id.indexOf('use_') != -1) { // one of the use_detection, use_tracing, etc check boxes
                 $(this).children('div').each(function (index, value) {
                     if( $(this).find('#' + use_check[0].id).length == 0) {//not my direct parent
                         if ( !use_check.is(':checked')) {
