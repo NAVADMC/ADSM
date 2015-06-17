3d2468e91032d61f385536b9bc7916356ad4a9b5
Josiah Seaman <josiah.seaman@gmail.com>
2015-06-17 15:06:45 -0600

    #330 Separate view for Functions

--- src/ScenarioCreator/views.py
+++ src/ScenarioCreator/views.py
@@ -438,9 +438,20 @@ def list_per_model(model_class):
     return context
 
 
+def functions_panel(request):
+    """Panel on the right that lists both Relational and Probability Functions with a graphic depiction"""
+    context = {'base_page': 'ScenarioCreator/ModelList.html',
+               'models': []}
+    for local_name, local_model in abstract_models['Function']:
+        context['models'].append(list_per_model(local_model))
+    return render(request, 'ScenarioCreator/Fragment.html', context)  # no 3 panel layout
+
+
 def model_list(request):
     model_name, model = get_model_name_and_model(request)
     model_name = promote_to_abstract_parent(model_name)
+    if model_name in 'Function RelationalFunction ProbabilityFunction'.split():
+        return functions_panel(request)
     if model_name == 'VaccinationTrigger':  # special case
         context = trigger_list(request)
     else:
@@ -452,9 +463,7 @@ def model_list(request):
                 context['models'].append(list_per_model(local_model))
         else:
             context['models'].append(list_per_model(model))
-    if model_name in 'Function RelationalFunction ProbabilityFunction'.split():
-        context.pop('title')
-        return render(request, 'ScenarioCreator/Fragment.html', context)  # no 3 panel layout
+
     return render(request, 'ScenarioCreator/3Panels.html', context)
 
 # Utility Views was moved to the ADSMSettings/connection_handler.py
