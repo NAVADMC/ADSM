ddd7772207b00ac775c81b6687d181e0a2ab16b7
Josiah Seaman <josiah.seaman@gmail.com>
2015-07-15 13:18:23 -0600

    #617 move probability-functions.js to adsm.js and replaced update functionality with livequery.  Edit Button styling is working correctly now

--- ADSM/static/js/adsm.js
+++ ADSM/static/js/adsm.js
@@ -312,6 +312,34 @@ $(document).on('submit', '.ajax', function(event) {
         $('.defined').removeClass('focused')
         $(this).addClass('focused');
     });
+
+    $('#id_equation_type').livequery(hide_unneeded_probability_fields)
+
+    $(document).on('change', '#id_equation_type', function(){
+        hide_unneeded_probability_fields();
+    });
+
+    $('.edit-button').livequery(function(){
+        $('.edit-button').on('click', function(){
+            $('#functions_panel .buttonHolder').removeAttr('hidden')
+            $('.edit-button-holder a, .edit-button-holder button').addClass('reveal')
+            //$('.edit-button').css('display', 'none') //TODO: fold out transition
+            $('#functions_panel, #functions_panel input').addClass('editable')
+            $('#functions_panel :input').addClass('editable')
+            //$('#tb_mask').css('visibility', 'visible')
+            $('#functions_panel').css('pointer-events','all') //TODO: necessary?
+        })
+    })
+
+    $('.edit-button-holder .copy-button').livequery(function() {
+        $('.edit-button-holder .copy-button').on('click', function () {
+            var target = $('#' + $(this).attr('form'))
+            target.attr('action', target.attr('action') + 'copy/')
+            console.log(target.attr('action'))
+            var name_in = $('#id_name')
+            name_in.val(name_in.val() + ' - Copy')
+        })
+    })
 })
 
 //#####################################################################################//
@@ -749,5 +777,23 @@ function ajax_submit_complex_form_and_replaceWith(formAction, formData, $self, l
         }
     }).always(function () {
         $('.blocking-overlay').hide();
+    });
+}
+
+function hide_unneeded_probability_fields() {
+    var $idEquationType = $('#id_equation_type');
+    var equation_type = $idEquationType.val()
+    var fields = $idEquationType.closest('.control-group').nextAll('.control-group');
+    fields.each(function (index, control_group) {
+        var help_text = $(control_group).find('.help-block').first().text();
+        var functions = help_text.toLowerCase().match(/(\w[\w\s]*)(?=[,\.])/g);
+        if (functions.indexOf(equation_type.toLowerCase()) >= 0) {
+            $(control_group).show();
+            $(control_group).find(':input').attr('required', 'required');
+        }
+        else {
+            $(control_group).hide();
+            $(control_group).find(':input').removeAttr('required');
+        }
     });
 }
