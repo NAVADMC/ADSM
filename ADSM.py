861b8b8c2603b1ba1b654ffeb4083754a4d05175
Bryan Hurst <bryan@newline.us>
2015-07-27 13:20:43 -0600

    #532 Update dance code.

--- ADSM.py
+++ ADSM.py
@@ -35,9 +35,24 @@ multiprocessing.freeze_support()
 parser = argparse.ArgumentParser(prog='adsm.exe')
 # TODO: Tests don't run currently as the test runner won't find compiled tests.
 parser.add_argument('-t', '--test', dest='test', help='run the test suite', action='store_true')
-parser.add_argument('-s', '--skip_update', dest='skip_update', help='do not check for updates', action='store_true')
 parser.add_argument('-n', '--update_name', dest='update_name', help='Query for the name of this program as known to the update server', action='store_true')
+parser.add_argument('-u', '--update', dest='update', help='Launch the updater after checking for new versions of the update client', action='store_true')
 args = parser.parse_args()
+
+
+def launch_external_program_and_exit(launch, code=0, launch_args=None):
+    if not launch_args:
+        launch_args = {}
+    if sys.platform == 'win32':  # Yes, this is also x64.
+        CREATE_NEW_PROCESS_GROUP = 0x00000200
+        DETACHED_PROCESS = 0x00000008
+        launch_args.update(creationflags=DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP)
+    else:
+        launch_args.update(preexec_fn=os.setsid)
+        launch_args.update(start_new_session=True)
+    subprocess.Popen([launch], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **launch_args)
+    sys.exit(code)
+
 
 print("Checking with the update service...")
 # Update the updater
@@ -45,8 +60,11 @@ if os.path.exists(os.path.join(BASE_DIR, 'npu.exe.updated')):
     if os.path.exists(os.path.join(BASE_DIR, 'npu.exe')):
         os.remove(os.path.join(BASE_DIR, 'npu.exe'))
     os.rename(os.path.join(BASE_DIR, 'npu.exe.updated'), os.path.join(BASE_DIR, 'npu.exe'))
+# Launch the update client
+if args.update:
+    launch_external_program_and_exit(launch=os.path.join(BASE_DIR, 'npu.exe'))
 # Respond to an updater query
-if args.update_name:
+elif args.update_name:
     print("ADSM")
     sys.exit(0)
 
