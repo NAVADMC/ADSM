1118f91e6630098bf8bbde4a534076cedce0a0be
Bryan Hurst <bryan@newline.us>
2015-07-30 12:37:01 -0600

    Put version information in the context.

--- ADSMSettings/context_processor.py
+++ ADSMSettings/context_processor.py
@@ -4,17 +4,18 @@ from ADSMSettings.models import SmSession
 from ADSMSettings.models import unsaved_changes
 from ADSMSettings.utils import workspace_path, file_list, scenario_filename, npu_update_info
 
+from ADSM import __version__
+
 
 def adsm_context(request):
     context = {}
     if request.path and request.path != '/' and '/LoadingScreen/' not in request.path:
         context = {'filename': scenario_filename(),  # context in either mode
                    'unsaved_changes': unsaved_changes(),
-                   'update_available': SmSession.objects.get().update_available,
                    'url': request.path,
                    'active_link': '/'.join(re.split('\W+', request.path)[2:]),
-                   'dev_version': 'Current Version',
-                   'update_version': 'Newer version',
+                   'dev_version': __version__,
+                   'update_version': SmSession.objects.get().update_available,
                    'workspace_path': workspace_path(),
                    'db_files': (file_list(".sqlite3")),
         }
