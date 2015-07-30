3393c03a91c06f85bde2e0363c3546d943e8570f
Bryan Hurst <bryan@newline.us>
2015-07-30 11:33:00 -0600

    Fix version number for pip. Update test db with new version migrations.

--- ADSM/__init__.py
+++ ADSM/__init__.py
@@ -1,1 +1,1 @@
-__version__ = '1.1b4'  # Valid values are x.x.x (final release) x.xax (alpha release) x.xbx (beta release) x.xrcx (release candidate release)
+__version__ = '1.1.0.4'  # Major.Minor.Bug[.beta]
