99557fe17ff87655cfa2bd52f4c3239f89cdf2c9
ndh2 <ndh2@users.noreply.github.com>
2015-09-04 09:07:19 -0400

    Fix for the problem where PDFs would get combined names like "No airborne delay, 0 day delay, 0 day delay, 0 day delay 0 day delay etc..." #459
    When the next release goes out, I will apply the User Testing label and re-assign this issue.

--- ADSMSettings/xml2sqlite.py
+++ ADSMSettings/xml2sqlite.py
@@ -28,10 +28,13 @@ def status(message):
 def create_no_duplicates(ModelClass, suggested_name, **kwargs):
     instance, created = ModelClass.objects.get_or_create(**kwargs)
     if suggested_name is not None:  # TODO: use Django _meta to check for a field called "name"
+        # Remove comma from suggested_name because we use commas as a separator
+        suggested_name = suggested_name.replace(',','')
         if created:
             instance.name = suggested_name
         else:
-            if instance.name != suggested_name:
+            current_name_parts = set(instance.name.split(', '))
+            if suggested_name not in current_name_parts:
                 instance.name += ', ' + suggested_name
         instance.save()
     return instance, created
