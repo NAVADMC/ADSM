c42fe430161c682c0b96e16b65fe09423124b43c
Josiah Seaman <josiah.seaman@gmail.com>
2015-11-18 16:02:29 -0700

    #676 Better React Test

--- ADSM/static/js/adsm-react.js
+++ ADSM/static/js/adsm-react.js
@@ -1,19 +1,26 @@
 'use strict';
 
-var $ = require('jquery');  
+var $ = require('jquery');
 var React = require('react');
 
-var TestApp = React.createClass({  
-  render: function() {
-    return (
-      <div className="page">
-        <h1>React test!</h1>
-      </div>
-    );
-  }
+var TestApp = React.createClass({
+    getInitialState: function(){
+        return {clicked:false};
+    },
+    onClick: function(){
+        this.setState({clicked: !this.state.clicked});
+    },
+    render: function() {
+        var content = this.state.clicked ? "Clicked" :"React test!";
+        return (
+            <div className="page">
+                <h1 onClick={this.onClick}>{content}</h1>
+            </div>
+        );
+    }
 });
 
-React.render(  
-  React.createElement(TestApp, null),
-  document.getElementById('content')
+React.render(
+    React.createElement(TestApp, null),
+    document.getElementById('content')
 );
