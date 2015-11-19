dd04739884f38eb49e7e95d426bfee96f1ba959f
Bryan Hurst <bryan@newline.us>
2015-11-19 09:47:09 -0700

    #680 Import Django Debug Toolbar and do basic configuration. Import Pympler for memory tracking and integrate it with Django Debug Toolbar. Enable profiling middleware and integrate with Django Debug Toolbar.

--- ADSM/urls.py
+++ ADSM/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
+from django.conf import settings
 
 
 admin.autodiscover()
@@ -16,6 +17,10 @@ urlpatterns = patterns('',
     url('^500/$', 'ADSM.debug_views.handler500'),
     url(r'react/$', TemplateView.as_view(template_name='react.html'))
     )
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('', url(r'^__debug__/', include(debug_toolbar.urls)), )
 
 handler400 = 'ADSM.debug_views.handler400'
 handler403 = 'ADSM.debug_views.handler403'
