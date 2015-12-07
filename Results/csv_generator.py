e4b835b3417bb4d94233cea747ba05319c81f406
Josiah Seaman <josiah.seaman@gmail.com>
2015-12-07 10:52:25 -0700

    #668 Generating data

--- Results/csv_generator.py
+++ Results/csv_generator.py
@@ -2,8 +2,9 @@ import multiprocessing
 import csv
 
 from django.conf import settings
-
+from django.db.models import Avg, Min, Max
 from ADSMSettings.utils import workspace_path, scenario_filename
+from Results.output_grammar import explain
 
 
 def create_csv_file(location, headers, data):
@@ -12,6 +13,11 @@ def create_csv_file(location, headers, data):
 
         writer.writeheader()
         [writer.writerow(x) for x in data]
+
+
+def django_percentile(field_name, query_set, cutoff_percentile, count):
+    index = (count - 1) * (cutoff_percentile / 100)
+    return query_set.order_by(field_name)[index]
 
 
 class SummaryCSVGenerator(multiprocessing.Process):
@@ -30,9 +36,10 @@ class SummaryCSVGenerator(multiprocessing.Process):
                 settings.DATABASES[database]['NAME'] = settings.DATABASES[database]['TEST']['NAME'] if 'TEST' in settings.DATABASES[database] else settings.DATABASES[database]['TEST_NAME']
 
         location = workspace_path(scenario_filename() + '/summary.csv')  # Note: scenario_filename uses the database
-        headers, data = ["test1", "test2"], [{'test1': "1blah1", 'test2': "1blah2"}, {'test1': "2blah1", 'test2': "2blah2"}]  # TODO: Use class method to get these
+        headers, data = self.get_summary_data_table()
 
         create_csv_file(location, headers, data)
+
 
     def get_summary_data_table(self):
         """Generates a python data structure with all the information for the CSV file.  Structured in a list[row][column] 2D array that mimics the spreadsheet
@@ -40,10 +47,46 @@ class SummaryCSVGenerator(multiprocessing.Process):
         and retreival, but it's simplest to just calculate and store them in order."""
         from Results.models import DailyByProductionType
         fields_of_interest = [] # only cumulative, last day fields in DailyByProductionType for all production types
-        DailyByProductionType.objects.filter(last_day=True, production_type__isnull=True, )
+        query_set = DailyByProductionType.objects.filter(last_day=True, production_type__isnull=True, )
+        count = query_set.count()  # only needs to be evaluated once
         for field, val in DailyByProductionType():
             if 'c' in field:
                 fields_of_interest.append(field)
+        headers = ['Field Name', 'Explanation', 'Mean', 'StdDev', 'Low', 'High', 'p5', 'p25', 'p50', 'p75', 'p95']
+        data = []  # 2D
+
+        for field_name in fields_of_interest:
+            data.append(self.summary_row(field_name, query_set, headers, count))
+
+        return ','.join(headers), data
 
 
-        pass
+    def summary_row(self, field_name, query_set, headers, count):
+
+        resolvers = {'Field Name': lambda field, query: field_name,
+                     'Explanation': lambda field, query: explain(field_name),
+                     'Mean': lambda field, query: query.aggregate(Avg(field)),
+                     'StdDev': lambda field, query: 1.0,
+                     'Low': lambda field, query: query.aggregate(Min(field)),
+                     'High': lambda field, query: query.aggregate(Max(field)),
+                     'p5': lambda field, query: django_percentile(field, query, 5, count),
+                     'p25': lambda field, query: django_percentile(field, query, 25, count),
+                     'p50': lambda field, query: django_percentile(field, query, 50, count),
+                     'p75': lambda field, query: django_percentile(field, query, 75, count),
+                     'p95': lambda field, query: django_percentile(field, query, 95, count),
+        }
+        row = []
+        for column in headers:
+            if column in resolvers.keys():
+                row.append(resolvers[column](field_name, query_set))
+            else:
+                raise NotImplemented("The column name " + column + " does not have a function associated with it.")
+
+        return row
+
+
+
+
+
+
+
