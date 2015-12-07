d0534e24123316f104c10befba170537da8e3d16
Josiah Seaman <josiah.seaman@gmail.com>
2015-12-07 10:03:00 -0700

    #668 Iterating over fields

--- Results/csv_generator.py
+++ Results/csv_generator.py
@@ -19,3 +19,17 @@ class SummaryCSVGenerator(multiprocessing.Process):
                 settings.DATABASES[database]['NAME'] = settings.DATABASES[database]['TEST']['NAME'] if 'TEST' in settings.DATABASES[database] else settings.DATABASES[database]['TEST_NAME']
 
         # TODO: Generate the Statistics Summary Report CSV File
+
+    def get_summary_data_table(self):
+        """Generates a python data structure with all the information for the CSV file.  Structured in a list[row][column] 2D array that mimics the spreadsheet
+        layout and follows the column order.  This could be changed to an OrderedDict( OrderedDict<column, value> ) if you want more flexibility in storage
+        and retreival, but it's simplest to just calculate and store them in order."""
+        from Results.models import DailyByProductionType
+        fields_of_interest = [] # only cumulative, last day fields in DailyByProductionType for all production types
+        DailyByProductionType.objects.filter(last_day=True, production_type__isnull=True, )
+        for field, val in DailyByProductionType():
+            if 'c' in field:
+                fields_of_interest.append(field)
+
+
+        pass
