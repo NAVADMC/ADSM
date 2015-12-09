7c92aeb4de49d508f130b1284c0c9dbfe14985f4
Bryan Hurst <bryan@newline.us>
2015-12-09 09:13:32 -0700

    Setup webpack to allow for multiple entry points.

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,35 @@
 var path = require("path");
 var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
+var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
-module.exports = {
-  context: __dirname,
+    module.exports = {
+    context: __dirname,
 
-  entry: './ADSM/static/js/adsm-react', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+    entry: {
+        adsmReact: './ADSM/static/js/adsm-react',
+        adsmReact2: './ADSM/static/js/adsm-react2'
+    },
 
-  output: {
-      path: path.resolve('./ADSM/static/bundles/'),
-      filename: "[name].js",
-  },
+    output: {
+        path: path.resolve('./ADSM/static/bundles/'),
+        filename: "[name].js",
+        chunkFilename: "[name].chunk[id].js"
+    },
 
-  plugins: [
-    new BundleTracker({filename: './webpack-stats.json'}),
-  ],
-
-  module: {
-    loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', query: {presets:['es2015', 'react']}}, // to transform JSX into JS
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'}),
+        new CommonsChunkPlugin("commons.js")
     ],
-  },
 
-  resolve: {
-    modulesDirectories: ['node_modules', 'bower_components'],
-    extensions: ['', '.js', '.jsx']
-  },
-}
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', query: {presets:['es2015', 'react']}}, // to transform JSX into JS
+        ]
+    },
+
+    resolve: {
+        modulesDirectories: ['node_modules', 'bower_components'],
+        extensions: ['', '.js', '.jsx']
+    }
+};
