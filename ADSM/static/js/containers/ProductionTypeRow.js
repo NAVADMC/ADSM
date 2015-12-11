40fe5d9d44a3af60f7f1bfd880b03b4a43a84b49
Josiah Seaman <josiah@newline.us>
2015-12-10 17:19:42 -0700

    #586 plain pointer

--- ADSM/static/js/containers/ProductionTypeRow.js
+++ ADSM/static/js/containers/ProductionTypeRow.js
@@ -12,7 +12,7 @@ export default class ProductionTypeRow extends Component {
         return(
             <li>
                 <div className="population-super-column">
-                    <a href="#">{ name }</a>
+                    <div href="#">{ name }</div>
                     <div className="unit-count">{units}</div>
                 </div>
                 <div title={progression}         className="productiontypes-header"><div className={progression ? css + ' filled' : css} /></div>
