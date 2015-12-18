9d7edfff5f3598a1499853015cb0f3f8d1b9844f
Josiah Seaman <josiah@newline.us>
2015-12-18 11:27:34 -0700

    Fixes #697 by preventing the default mousedown event behavior that deselects all the other options

--- ADSM/static/js/population-panel.js
+++ ADSM/static/js/population-panel.js
@@ -1,11 +1,14 @@
 /** Created by Josiah on 2/18/2015. */
 
 function toggle(element, attribute){
-    if($(element).attr(attribute)) {
+    if($(element).prop(attribute) || $(element).attr(attribute)) {
+        $(element).prop(attribute, false);
         $(element).removeAttr(attribute, false);
     } else {
+        $(element).prop(attribute, true);
         $(element).attr(attribute, attribute);
     }
+
 }
 
 function toggle_added_state(text) {
@@ -20,17 +23,15 @@ function select_production_type(text) {
         $(selector).each(function() {
             if ($(this).text() == text) {
                 toggle(this, 'selected')
+                $(this).closest('.layout-panel').find('.btn-save').removeAttr('disabled') //unsaved changes
             }
         })
     });
     toggle_added_state(text);
 }
 
-//child has selected attr, then remove .empty  has .productiontypelist
-//on load have .empty
-//editing existing 
-
-$(document).on('click', '#ProductionTypes li, #ProductionGroups li, .productiontypelist option', function(event){
+$(document).on('mousedown', '#ProductionTypes li, #ProductionGroups li, .productiontypelist option', function(event){
+    //It's really critical this is mousedown and not click.  the default mousedown event behavior deselects all the other options
     event.preventDefault()
     var text = $(this).text();
     if(this.tagName == 'LI'){  // I want the click target to be the whole row, but the name is defined in pt-name
