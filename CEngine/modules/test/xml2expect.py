105ca1bc6b770a4d6ffd1d89b306dd558d3ca83a
ndh2 <ndh2@users.noreply.github.com>
2016-02-29 19:34:19 -0500

    Updated 3 scripts for compatibility with Python 3. Now developers won't need to use Python 2 inside the CEngine folder and Python 3 everywhere else in the project. Closes #726

--- CEngine/modules/test/xml2expect.py
+++ CEngine/modules/test/xml2expect.py
@@ -14,10 +14,6 @@ __date__ = "December 2003"
 
 import sys
 import xml.dom.minidom
-import operator
-import string
-
-OUTPUT_ENCODING = "utf-8" # to match output from the simulator
 
 
 
@@ -32,14 +28,6 @@ def getText (element):
 
 
 
-def all (X):
-	"""Returns true if all items in a sequence are true."""
-	if len(X) == 0:
-		return False
-	return reduce (operator.__and__, map (lambda x:not not x, X))
-
-
-
 def rowToStates (row, state_code):
 	"""Returns an array of state codes given a row of <td> elements from the
 	input file.  Accepts 1-letter codes (e.g. "S"), long names (e.g.
@@ -49,7 +37,7 @@ def rowToStates (row, state_code):
 		state = getText (cell)
 		if state in state_code.values():
 			states.append (state) # this is a valid 1-letter code
-		elif state_code.has_key(state):
+		elif state in state_code:
 			states.append (state_code[state]) # convert long name to 1-letter code
 		else:
 			# Break into chars, see if each is a valid 1-letter code
@@ -57,7 +45,7 @@ def rowToStates (row, state_code):
 			if all([char in state_code.values() for char in nospaces]):
 				states += [char for char in nospaces]
 			else:
-				raise ValueError, ('"%s" is not a recognized state' % state)
+				raise ValueError('"%s" is not a recognized state' % state)
 	# end of loop over <td> elements
 	return states
 
@@ -66,7 +54,7 @@ def rowToStates (row, state_code):
 def main ():
 	doc = xml.dom.minidom.parse (sys.stdin)
 	# Boilerplate at the top of the file.
-	print r"""set timeout 3
+	print(r"""set timeout 3
 #
 # expectations that clean up in case of error. Note that `$test' is
 # a purely local variable.
@@ -87,7 +75,7 @@ expect_after {
 	timeout {
 		fail "$test (timeout)"
 	}
-}"""
+}""")
 
 	state_code = {
 	  'Susceptible': 'S', # map long names to short ones
@@ -103,133 +91,133 @@ expect_after {
 	tests = doc.getElementsByTagName ("deterministic-test")
 	for test in tests:
 		description = getText (test.getElementsByTagName ("description")[0])
-		print '\n\n\n#'
+		print('\n\n\n#')
 		for line in description.split('\n'):
-			print '# %s' % line.strip().encode(OUTPUT_ENCODING, "replace")
-		print '#'
+			print('# %s' % line.strip())
+		print('#')
 
 		parameters = getText (test.getElementsByTagName ("parameter-description")[0])
 		for line in parameters.split('\n'):
-			print '# %s' % line.strip().encode(OUTPUT_ENCODING, "replace")
-		print '#'
+			print('# %s' % line.strip())
+		print('#')
 
 		category = getText (test.getElementsByTagName ("category")[0])
 		parameterFileName = getText (test.getElementsByTagName ("parameter-file")[0])
 		populationFileName = getText (test.getElementsByTagName ("population-file")[0])
-		print 'set scenario "test/module.%s/%s_%s.sqlite3"' % (category, parameterFileName, populationFileName)
+		print('set scenario "test/module.%s/%s_%s.sqlite3"' % (category, parameterFileName, populationFileName))
 
 		table = test.getElementsByTagName ("output")[0]
-		print 'set states {'
+		print('set states {')
 		for row in table.getElementsByTagName ("tr"):
-			print '  {', string.join (rowToStates (row, state_code)), '}'
-		print '}'
+			print('  {', ' '.join(rowToStates (row, state_code)), '}')
+		print('}')
 
 		shortName = getText (test.getElementsByTagName ("short-name")[0])
-		print 'progress_test $scenario $states "%s"' % shortName
+		print('progress_test $scenario $states "%s"' % shortName)
 
 	# Print the stochastic herd-state tests.
 	tests = doc.getElementsByTagName ("stochastic-test")
 	for test in tests:
 		description = getText (test.getElementsByTagName ("description")[0])
-		print '\n\n\n#'
+		print('\n\n\n#')
 		for line in description.split('\n'):
-			print '# %s' % line.strip().encode(OUTPUT_ENCODING, "replace")
-		print '#'
+			print('# %s' % line.strip())
+		print('#')
 
 		parameters = getText (test.getElementsByTagName ("parameter-description")[0])
 		for line in parameters.split('\n'):
-			print '# %s' % line.strip().encode(OUTPUT_ENCODING, "replace")
-		print '#'
+			print('# %s' % line.strip())
+		print('#')
 
 		category = getText (test.getElementsByTagName ("category")[0])
 		parameterFileName = getText (test.getElementsByTagName ("parameter-file")[0])
 		populationFileName = getText (test.getElementsByTagName ("population-file")[0])
-		print 'set scenario "test/module.%s/%s_%s.sqlite3"' % (category, parameterFileName, populationFileName)
+		print('set scenario "test/module.%s/%s_%s.sqlite3"' % (category, parameterFileName, populationFileName))
 
 		tables = test.getElementsByTagName ("output")
-		print 'set states {'
+		print('set states {')
 		for table in tables:
-			print '  {', table.getAttribute ("probability")
+			print('  {', table.getAttribute ("probability"))
 			for row in table.getElementsByTagName ("tr"):
-				print '  {', string.join (rowToStates (row, state_code)), '}'
-			print '  }'
-		print '}'
+				print('  {', ' '.join(rowToStates (row, state_code)), '}')
+			print('  }')
+		print('}')
 
 		shortName = getText (test.getElementsByTagName ("short-name")[0])
-		print 'stochastic_progress_test $scenario $states "%s"' % shortName
+		print('stochastic_progress_test $scenario $states "%s"' % shortName)
 
 	# Print the deterministic output variable tests.
 	tests = doc.getElementsByTagName ("variable-test")
 	for test in tests:
 		description = getText (test.getElementsByTagName ("description")[0])
-		print '\n\n\n#'
+		print('\n\n\n#')
 		for line in description.split('\n'):
-			print '# %s' % line.strip().encode(OUTPUT_ENCODING, "replace")
-		print '#'
+			print('# %s' % line.strip())
+		print('#')
 
 		parameters = getText (test.getElementsByTagName ("parameter-description")[0])
 		for line in parameters.split('\n'):
-			print '# %s' % line.strip().encode(OUTPUT_ENCODING, "replace")
-		print '#'
+			print('# %s' % line.strip())
+		print('#')
 
 		category = getText (test.getElementsByTagName ("category")[0])
 		parameterFileName = getText (test.getElementsByTagName ("parameter-file")[0])
 		populationFileName = getText (test.getElementsByTagName ("population-file")[0])
-		print 'set scenario "test/module.%s/%s_%s.sqlite3"' % (category, parameterFileName, populationFileName)
+		print('set scenario "test/module.%s/%s_%s.sqlite3"' % (category, parameterFileName, populationFileName))
 
 		table = test.getElementsByTagName ("output")[0]
 		rows = table.getElementsByTagName ("tr")
 		varnames = [getText (cell) for cell in rows[0].getElementsByTagName ("td")]
 		del rows[0]
-		print 'set values {'
+		print('set values {')
 		for i in range (len (varnames)):
-			print ('  { "%s"' % varnames[i].encode(OUTPUT_ENCODING, "replace")),
+			print('  { "%s"' % varnames[i], end=' ')
 			for row in rows:
 				cell = row.getElementsByTagName ("td")[i]
-				print getText (cell) or '""',
-			print '}'
-		print '}'
+				print(getText (cell) or '""', end=' ')
+			print('}')
+		print('}')
 
 		shortName = getText (test.getElementsByTagName ("short-name")[0])
-		print 'variable_test $scenario $values "%s"' % shortName
+		print('variable_test $scenario $values "%s"' % shortName)
 
 	# Print the stochastic output variable tests.
 	tests = doc.getElementsByTagName ("stochastic-variable-test")
 	for test in tests:
 		description = getText (test.getElementsByTagName ("description")[0])
-		print '\n\n\n#'
+		print('\n\n\n#')
 		for line in description.split('\n'):
-			print '# %s' % line.strip().encode(OUTPUT_ENCODING, "replace")
-		print '#'
+			print('# %s' % line.strip())
+		print('#')
 
 		parameters = getText (test.getElementsByTagName ("parameter-description")[0])
 		for line in parameters.split('\n'):
-			print '# %s' % line.strip().encode(OUTPUT_ENCODING, "replace")
-		print '#'
+			print('# %s' % line.strip())
+		print('#')
 
 		category = getText (test.getElementsByTagName ("category")[0])
 		parameterFileName = getText (test.getElementsByTagName ("parameter-file")[0])
 		populationFileName = getText (test.getElementsByTagName ("population-file")[0])
-		print 'set scenario "test/module.%s/%s_%s.sqlite3"' % (category, parameterFileName, populationFileName)
+		print('set scenario "test/module.%s/%s_%s.sqlite3"' % (category, parameterFileName, populationFileName))
 		tables = test.getElementsByTagName ("output")
 
-		print 'set values {'
+		print('set values {')
 		for table in tables:
-			print '  {', table.getAttribute ("probability")
+			print('  {', table.getAttribute ("probability"))
 			rows = table.getElementsByTagName ("tr")
 			varnames = [getText (cell) for cell in rows[0].getElementsByTagName ("td")]
 			del rows[0]
 			for i in range (len (varnames)):
-				print ('    { "%s"' % varnames[i]),
+				print('    { "%s"' % varnames[i], end=' ')
 				for row in rows:
 					cell = row.getElementsByTagName ("td")[i]
-					print getText (cell) or '""',
-				print '}'
-			print '  }'
-		print '}'
+					print(getText (cell) or '""', end=' ')
+				print('}')
+			print('  }')
+		print('}')
 
 		shortName = getText (test.getElementsByTagName ("short-name")[0])
-		print 'stochastic_variable_test $scenario $values "%s"' % shortName
+		print('stochastic_variable_test $scenario $values "%s"' % shortName)
 
 
 
