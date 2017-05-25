29b682cdc6e49b9662b23bfae8684643ac8de3d4
Bryan Hurst <bryan@newline.us>
2017-05-25 16:10:17 -0600

    Merge migration conflict.

---
+++ ScenarioCreator/migrations/0021_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ScenarioCreator', '0016_auto_20170406_0926'),
+        ('ScenarioCreator', '0020_remove_old_vacc_priority_order_settings'),
+    ]
+
+    operations = [
+    ]
