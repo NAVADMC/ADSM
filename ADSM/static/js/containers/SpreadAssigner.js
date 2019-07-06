e9a7a80ca4134e129d720634383f57d096f58294
Conrad Selig <31974507+ConradSelig@users.noreply.github.com>
2019-07-06 01:02:11 -0600

    #851 Changes to production type combinations widget.

--- ADSM/static/js/containers/SpreadAssigner.js
+++ ADSM/static/js/containers/SpreadAssigner.js
@@ -12,45 +12,37 @@ import SelectBox from 'react-select-box'
 export default class SpreadAssigner extends Component {
 
     onChangeSource(event) {
-        var {spread_type, pk, input_state, index} = this.props
+        var {spread_type, pk, input_state, index} = this.props;
         var new_value = event.target.value;
-        new_value = Object.assign({}, input_state, {source: new_value})  //wrap in the old values
+        new_value = Object.assign({}, input_state, {source: new_value});  //wrap in the old values
         dispatch(select_value_changed(spread_type, pk, index, 'source', new_value, input_state))
     }
 
     onChangeDestinations(new_destinations) {
-        var {spread_type, pk, input_state, index} = this.props
+        var {spread_type, pk, input_state, index} = this.props;
         //TODO: check if number is already in the list and remove it if it is
-        var new_value = Object.assign({}, input_state, {destinations: new_destinations})  //wrap in the old values
+        var new_value = Object.assign({}, input_state, {destinations: new_destinations});  //wrap in the old values
         dispatch(select_value_changed(spread_type, pk, index, 'destinations', new_value, input_state))
     }
 
     render(){
-        var { population, spread_type, pk, input_state } = this.props
+        var { population, spread_type, pk, input_state } = this.props;
         var options = population.map(function(option, index){
             return <option value={option.pk} key={'s'+index}>{option.name}</option>;
-        })
+        });
         var destination_options = population.map(function(option, index){
             return <option value={option.pk} key={'d'+index}>{option.name}</option>;
-        })
+        });
         return (
             <div>
-                <label htmlFor="source" className="control-label requiredField">
-				    Source Production Type<span className="asteriskField">*</span>
+                <strong>
+                    Source Production Type:
+                </strong>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{(population[input_state.source - 1]).name}</p>
+                <label htmlFor="destinations" className="control-label">
+				    Destinations:
                 </label>
-                <select name="source"
-                        required
-                        value={input_state.source}
-                        onChange={this.onChangeSource.bind(this)}
-                        key="source">
-                    <option value="">------</option>
-                    {options}
-                </select>
-
-                <label htmlFor="destinations" className="control-label requiredField">
-				    Destinations<span className="asteriskField">*</span>
-                </label>
-                <SelectBox name="destinations" required
+                <SelectBox name="destinations"
                            spread_type={spread_type}
                            pk={pk}
                            multiple={true}
