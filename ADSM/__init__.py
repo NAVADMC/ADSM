d755b5c1bc60ad1a7ab9f648adb7e34286baba49
Bryan Hurst <bryan@newline.us>
2019-12-03 13:06:42 -0700

    Version bump

--- ADSM/__init__.py
+++ ADSM/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '3.5.10.10'  # SimulationMajor.SimulationMinor.UIRelease.UIMinor/Beta
+__version__ = '3.5.10.11'  # SimulationMajor.SimulationMinor.UIRelease.UIMinor/Beta
 __year__ = '2019'
