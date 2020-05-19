d18a9f8f571ca1d4ca1ea021f9ab4f047527515b
ConradSelig <conrad.selig@mines.sdsmt.edu>
2020-05-19 03:33:42 -0600

    #1001 Added code to collect the names of all the scenarios that need to be run.

--- auto_runner.py
+++ auto_runner.py
@@ -1,5 +1,9 @@
-from ADSM import settings
 from CSUtils import args2dict
+from itertools import chain
+from ADSM import settings
+from glob import glob
+
+import os
 
 
 def setup(args):
@@ -13,6 +17,11 @@ def setup(args):
     parsed_args = {}
     # file object for the exclude file
     exclude_file = ""
+    # list of all scenarios to process
+    scenarios = []
+    # list of scenarios to exclude (if applicable)
+    excluded_scenarios = []
+    file_text = ""  # raw file data
 
     # parse the given arguments into that dictionary
     parsed_args = args2dict(args)
@@ -68,13 +77,67 @@ def setup(args):
         # but their arguments had a typo they know that their exclude file was not used.
         print("No exclude file given, running all scenarios.")
 
+    if parsed_args["v"]:
+        print("[setup] Building list of scenarios to run...")
+
+    # first we need to create a list of excluded scenarios
+    # if an exclude_file was even given
+    if exclude_file is not None:
+        # get the file data
+        file_text = exclude_file.readlines()
+        # build the exclusion list be removing newlines
+        excluded_scenarios = [file_name.replace("\n", "") for file_name in file_text]
+
+    # for every folder in the given workspace
+    for potential in os.listdir(parsed_args["scenarios_path"]):
+        if parsed_args["v"]:
+            print("\n[setup] Checking next potential scenario: " + potential)
+
+        # make sure the potential is not excluded, no sense verifying it if we can't use it anyways
+        if potential in excluded_scenarios:
+            if parsed_args["v"]:
+                print("[setup] Potential scenario excluded by user. Skipping.")
+            continue
+
+        # try catch block will prevent anything not a directory in the workspace from stopping ADSM
+        try:
+            # get all of the files within the potential scenario
+            potential_files = os.listdir(parsed_args["scenarios_path"] + "/" + potential)
+
+            # hard-check to make sure we're not in the settings folder
+            if 'activeSession.db' in potential_files and 'settings.db' in potential_files:
+                if parsed_args["v"]:
+                    print("[setup] Settings folder detected. Skipping.")
+                continue
+
+            # for each file within the potential scenarios
+            for file in potential_files:
+                # if that file name has the .db extension
+                if ".db" in file:
+                    if parsed_args["v"]:
+                        print("[setup] Scenario confirmed, adding to list of known scenarios")
+                    scenarios.append(potential)
+                    break
+            else:
+                if parsed_args["v"]:
+                    print("[setup] Scenario denied.")
+        except NotADirectoryError:
+            if parsed_args["v"]:
+                print("[setup] Potential is not a directory. Skipping.")
+            continue
+
+    if parsed_args["v"]:
+        print("\n[setup] Collected scenarios:", scenarios, "\n")
+
+    parsed_args["scenarios"] = scenarios
+
     print("Setup complete, starting ADSM auto-runner.")
 
     # call the run function (with the opened exclude file, or None), this will actually start the auto-run process.
-    run(parsed_args, exclude_file)
+    run(parsed_args)
 
     if parsed_args["v"]:
-        print("[setup] ADSM auto-runner complete, cleaning up...")
+        print("\n[setup] ADSM auto-runner complete, cleaning up...")
 
     # if an exclude file was opened
     if "exclude_file" in parsed_args.keys():
@@ -89,14 +152,22 @@ def setup(args):
     return None
 
 
-def run(options, exclude_file):
+def run(options):
     """ auto-run main function. Loops through file in the current working directory and hands them to execute_next to
     actually run them through the simulator.
 
+    A scenario is defined as a folder within the given scenarios_path that has at least (but not exclusively) at .db
+    file within. The exception to this is the "settings" folder withing the default ADSM workspace, which contains
+    multiple .db files but is not considered a scenario.
+
     :param options: dictionary of formatted parameters
-    :param exclude_file: file object with a list of scenarios to skip (one scenario per line)
     :return: None
     """
+
+    # loop through all of the given scenarios
+    for scenario in options["scenarios"]:
+        print("Running next scenario: \"" + scenario + "\"")
+
     return None
 
 
